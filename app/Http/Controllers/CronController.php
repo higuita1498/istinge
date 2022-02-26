a9548e8b8f1990de178b7db40b8794120e8c043a
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-02-26 13:59:47 -0400

    Cron

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -33,7 +33,7 @@ class CronController extends Controller
         $grupos_corte = GrupoCorte::where('fecha_corte', $fecha_corte)->where('status', 1)->get();
         
         foreach($grupos_corte as $grupo_corte){
-            $contratos = Contrato::join('contactos as c', 'c.id', '=', 'contracts.client_id')->join('planes_velocidad as p', 'p.id', '=', 'contracts.plan_id')->join('inventario as i', 'i.id', '=', 'p.item')->join('empresas as e', 'e.id', '=', 'i.empresa')->select('contracts.id','contracts.public_id','c.id as cliente','contracts.state','contracts.fecha_corte','contracts.fecha_suspension','c.nombre','c.nit','c.celular','c.telefono1','p.name as plan', 'p.price','p.item','i.ref','e.terminos_cond','e.notas_fact')->where('contracts.grupo_corte',date($grupo_corte->id))->where('contracts.status',1)->get();
+            $contratos = Contrato::join('contactos as c', 'c.id', '=', 'contracts.client_id')->join('planes_velocidad as p', 'p.id', '=', 'contracts.plan_id')->join('inventario as i', 'i.id', '=', 'p.item')->join('empresas as e', 'e.id', '=', 'i.empresa')->select('contracts.id','contracts.public_id','c.id as cliente','contracts.state','contracts.fecha_corte','contracts.fecha_suspension','c.nombre','c.nit','c.celular','c.telefono1','p.name as plan', 'p.price','p.item','i.ref','e.terminos_cond','e.notas_fact')->where('contracts.grupo_corte',date($grupo_corte->id))->where('contracts.status',1)->where('contracts.state','enabled')->get();
             //dd($contratos);
             $num = Factura::where('empresa',1)->where('tipo',1)->orderby('nro','asc')->get()->last();
             if($num){
