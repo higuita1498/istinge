b7620be24383a8e693fe7066b651a4476b727844
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-03-03 09:07:16 -0400

    btn

--- resources/views/mikrotik/index.blade.php
+++ resources/views/mikrotik/index.blade.php
@@ -79,18 +79,18 @@
 						    <a title="Ver detalles" href="{{route('mikrotik.show',$mikrotik->id)}}" class="btn btn-outline-info btn-icons"><i class="fas fa-eye"></i></a>
 						    
 						    @if($mikrotik->status == 0)
-						        <button title="Conectar Mikrotik" class="btn btn-outline-success btn-icons" type="submit" onclick="confirmar('conectar-{{$mikrotik->id}}', '¿Está seguro que desea conectar la Mikrotik?', '');"><i class="fas fa-plug"></i></button>
+						        <button title="Conectar Mikrotik" class="btn btn-outline-success btn-icons" type="submit" onclick="confirmar('conectar-{{$mikrotik->id}}', '¿Está seguro que desea conectar la Mikrotik {{$mikrotik->nombre}}?', '');"><i class="fas fa-plug"></i></button>
 						        @else
-						        <button title="Desconectar Mikrotik" class="btn btn-outline-danger btn-icons" type="submit" onclick="confirmar('conectar-{{$mikrotik->id}}', '¿Está seguro que desea desconectar la Mikrotik?', '');"><i class="fas fa-plug"></i></button>
+						        <button title="Desconectar Mikrotik" class="btn btn-outline-danger btn-icons" type="submit" onclick="confirmar('conectar-{{$mikrotik->id}}', '¿Está seguro que desea desconectar la Mikrotik {{$mikrotik->nombre}}?', '');"><i class="fas fa-plug"></i></button>
 						    @endif
 						    @if($mikrotik->status == 1)
 						    <a href="{{ route('mikrotik.grafica',$mikrotik->id )}}" class="btn btn-outline-danger btn-icons" title="Gráfica de Consumo"><i class="fas fa-chart-area"></i></a>
 						    @if($mikrotik->status == 1 && $mikrotik->reglas == 0)
-						    <button title="Aplicar Reglas de Corte" class="btn btn-outline-dark btn-icons" type="submit" onclick="confirmar('regla-{{$mikrotik->id}}', '¿Está seguro que desea aplicar las reglas de corte a esta Mikrotik?', '');"><i class="fas fa-plus"></i></button>
+						    <button title="Aplicar Reglas de Corte" class="btn btn-outline-dark btn-icons" type="submit" onclick="confirmar('regla-{{$mikrotik->id}}', '¿Está seguro que desea aplicar las reglas de corte a esta Mikrotik {{$mikrotik->nombre}}?', '');"><i class="fas fa-plus"></i></button>
 						    @endif
 						    @endif
 						    @if($mikrotik->status == 0 && $mikrotik->uso() == 0)
-						        <button class="btn btn-outline-danger btn-icons" type="submit" title="Eliminar" onclick="confirmar('eliminar-mikrotik-{{$mikrotik->id}}', '¿Está seguro que deseas eliminar el Mikrotik?', 'Se borrará de forma permanente');"><i class="fas fa-times"></i></button>
+						        <button class="btn btn-outline-danger btn-icons" type="submit" title="Eliminar" onclick="confirmar('eliminar-mikrotik-{{$mikrotik->id}}', '¿Está seguro que deseas eliminar el Mikrotik {{$mikrotik->nombre}}?', 'Se borrará de forma permanente');"><i class="fas fa-times"></i></button>
 						    @endif
 						    <a title="IP's Autorizadas" href="{{ route('mikrotik.ips-autorizadas',$mikrotik->id )}}" class="btn btn-outline-warning btn-icons"><i class="fas fa-project-diagram"></i></a>
 						</td>
