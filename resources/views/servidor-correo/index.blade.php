487215af009464e4910b32c7eed1318bd8f24f1b
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-03-09 14:08:58 -0400

    fix

--- resources/views/servidor-correo/index.blade.php
+++ resources/views/servidor-correo/index.blade.php
@@ -29,15 +29,15 @@
 	    <input name="_method" type="hidden" value="PATCH">
 	    <div class="row">
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Servidor <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control"  id="servidor" name="servidor"  required="" value="{{$servidor->servidor}}" maxlength="200">
+	            <label class="control-label">Servidor</label>
+	            <input type="text" class="form-control"  id="servidor" name="servidor"   value="{{$servidor->servidor}}" maxlength="200">
 	            <span class="help-block error">
 	                <strong>{{ $errors->first('servidor') }}</strong>
 	            </span>
 	        </div>
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Seguridad <span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" name="seguridad" id="seguridad" title="Seleccione" required="">
+	            <label class="control-label">Seguridad</label>
+	            <select class="form-control selectpicker" name="seguridad" id="seguridad" title="Seleccione" >
 	                <option value="ssl" {{ $servidor->seguridad == 'ssl'?'selected':'' }}>SSL</option>
 	                <option value="tls" {{ $servidor->seguridad == 'tls'?'selected':'' }}>TLS</option>
 	            </select>
@@ -46,29 +46,29 @@
 	            </span>
 	        </div>
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Usuario <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control"  id="usuario" name="usuario"  required="" value="{{$servidor->usuario}}" maxlength="200">
+	            <label class="control-label">Usuario</label>
+	            <input type="text" class="form-control"  id="usuario" name="usuario"   value="{{$servidor->usuario}}" maxlength="200">
 	            <span class="help-block error">
 	                <strong>{{ $errors->first('usuario') }}</strong>
 	            </span>
 	        </div>
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Password <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control"  id="password" name="password"  required="" value="{{$servidor->password}}" maxlength="200">
+	            <label class="control-label">Password</label>
+	            <input type="text" class="form-control"  id="password" name="password"   value="{{$servidor->password}}" maxlength="200">
 	            <span class="help-block error">
 	                <strong>{{ $errors->first('password') }}</strong>
 	            </span>
 	        </div>
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Puerto <span class="text-danger">*</span></label>
-	            <input type="number" class="form-control"  id="puerto" name="puerto"  required="" value="{{$servidor->puerto}}" maxlength="200" min="0">
+	            <label class="control-label">Puerto</label>
+	            <input type="number" class="form-control"  id="puerto" name="puerto"   value="{{$servidor->puerto}}" maxlength="200" min="0">
 	            <span class="help-block error">
 	                <strong>{{ $errors->first('puerto') }}</strong>
 	            </span>
 	        </div>
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Estado <span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" name="estado" id="estado" title="Seleccione" required="">
+	            <label class="control-label">Estado</label>
+	            <select class="form-control selectpicker" name="estado" id="estado" title="Seleccione" >
 	                <option value="1" {{ $servidor->estado == '1'?'selected':'' }}>Habilitado</option>
 	                <option value="0" {{ $servidor->estado == '0'?'selected':'' }}>Deshabilitado</option>
 	            </select>
