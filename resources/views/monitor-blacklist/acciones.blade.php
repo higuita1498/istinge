5f7bbd0b7d7c24bc17033c208b84d705247456b0
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-03-25 14:00:16 -0400

    Grupos de Corte

--- resources/views/monitor-blacklist/acciones.blade.php
+++ resources/views/monitor-blacklist/acciones.blade.php
@@ -14,7 +14,7 @@
     <a href="{{route('monitor-blacklist.reporte', $id)}}" class="btn btn-outline-info btn-icons" title="Ver Reporte"><i class="fas fa-file-contract"></i></a>
 @endif
 @if(isset($session['758']))
-    <button type="button" class="btn btn-outline-success btn-icons" type="submit" title="Eliminar" onclick="confirmar('verificar{{$id}}', '¿Está seguro que desea verificar el blacklist?', 'Esto puede demorar unos minutos');"><i class="fas fa-check-double"></i></button>
+    <button type="button" class="btn btn-outline-success btn-icons" type="submit" title="Verificar IP" onclick="confirmar('verificar{{$id}}', '¿Está seguro que desea verificar el blacklist?', 'Esto puede demorar unos minutos');"><i class="fas fa-check-double"></i></button>
 @endif
 @if(isset($session['756']))
     <button type="button" class="btn btn-outline-danger btn-icons" type="submit" title="Eliminar" onclick="confirmar('eliminar{{$id}}', '¿Está seguro que desea eliminar el monitor blacklist?', 'Se borrara de forma permanente');"><i class="fas fa-times"></i></button>
