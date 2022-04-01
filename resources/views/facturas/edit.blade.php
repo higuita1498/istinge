863598bc421e2154a6c7168332257f9d6a5e2b4d
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-04-01 11:50:08 -0500

    ok tipo movimiento

--- resources/views/facturas/edit.blade.php
+++ resources/views/facturas/edit.blade.php
@@ -62,7 +62,7 @@
                     <div class="form-group row">
                         <label class="col-sm-4 col-form-label">Tipo movimiento <a><i data-tippy-content="Elige a que cuenta ira enlazado el movimiento contable" class="icono far fa-question-circle"></i></a></label>
                         <div class="col-sm-8">
-                            <select name="relacion" id="relacion" class="form-control selectpicker " title="Seleccione" data-live-search="true" data-size="5" required>
+                            <select name="relacion" id="relacion" class="form-control selectpicker " title="Seleccione" data-live-search="true" data-size="5">
                                 @foreach($relaciones as $relacion)
                                     <option value="{{$relacion->id}}">{{$relacion->codigo}} - {{$relacion->nombre}}</option>
                                 @endforeach
