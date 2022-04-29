1f2508b437bc8c19386380b304ff7c0e25474f0a
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-04-29 19:19:38 -0400

    fix

--- resources/views/configuracion/integracion_pasarela/show.blade.php
+++ resources/views/configuracion/integracion_pasarela/show.blade.php
@@ -65,7 +65,8 @@
 							<td class="bg-th">{{ $servicio->nombre == 'WOMPI' ? 'Llave pública' : 'API Key'}}</td>
 							<td>{{$servicio->api_key}}</td>
 						</tr>
-						@endif@if($servicio->api_event)
+						@endif
+						@if($servicio->api_event)
 						<tr>
 							<td class="bg-th">API Eventos</td>
 							<td>{{$servicio->api_event}}</td>
