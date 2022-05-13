d88cc9c111b5a2e2fd1248a2045f4894ebe8692c
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-05-13 16:39:31 -0400

    57

--- resources/views/configuracion/integracion_sms/edit.blade.php
+++ resources/views/configuracion/integracion_sms/edit.blade.php
@@ -32,7 +32,7 @@
 	        @endif
 
 	        <div class="col-md-4 form-group">
-	            <label class="control-label">Nro Celular para pruebas <span class="text-danger">*</span> <a><i data-tippy-content="Indique el número de celular para enviar el SMS de prueba, de ser un destino en Colombia, indíquelo sin el código de país (3XXXXXXXXX)" class="icono far fa-question-circle"></i></a></label>
+	            <label class="control-label">Nro Celular para pruebas <span class="text-danger">*</span> <a><i data-tippy-content="Indique el número de celular para enviar el SMS de prueba, de ser un destino en Colombia, indíquelo con el código de país (57XXXXXXXXX)" class="icono far fa-question-circle"></i></a></label>
 	            <input type="number" class="form-control" id="numero" name="numero"  required="" value="{{$servicio->numero}}" maxlength="200" min="0">
 	            <span class="help-block error">
 	                <strong>{{ $errors->first('numero') }}</strong>
