4b886bb897c6504ac49c8040b148dbd681fd2527
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-05-14 12:25:27 -0400

    Efecty

--- app/Http/Controllers/IngresosController.php
+++ app/Http/Controllers/IngresosController.php
@@ -1090,7 +1090,7 @@ class IngresosController extends Controller
             fclose($gestor);
 
             foreach ($registros as $registro) {
-                $codigo = substr($registro['8'],1,-2);
+                $codigo = substr($registro['8'],1,-3);
                 $precio = $registro['2'];
                 $factura = Factura::where('codigo', $codigo)->first();
                 if($factura){
