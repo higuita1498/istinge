3f752d8ab1b4fd8e0f314741e7305efcc85e94e4
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-05-25 12:00:39 -0500

    factura estandar a factura electronica 100%

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -3293,6 +3293,7 @@ class FacturasController extends Controller{
         $factura->codigo=$nro->prefijo.$inicio;
         $factura->nro = $numero;
         $factura->numeracion = $nro->id;
+        $factura->tipo = 2;
         $factura->save();
 
         return back()->with('success','Factura con el nuevo código: '.$factura->codigo. ' convertida correctamente.');
