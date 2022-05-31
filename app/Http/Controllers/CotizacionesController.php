2d06aef82c37697feb3f68210df9a7ea9a23db8d
AJ23ThemeMaster <frankjmarvala@gmail.com>
2022-05-31 15:53:48 -0400

    CotizacionesController.php

--- app/Http/Controllers/CotizacionesController.php
+++ app/Http/Controllers/CotizacionesController.php
@@ -137,29 +137,6 @@ class CotizacionesController extends Controller
      * @return redirect
      */
     public function store(Request $request){
-        if( Cotizacion::where('empresa',auth()->user()->empresa)->count() > 0){
-                     //Tomamos el tiempo en el que se crea el registro
-        Session::put('posttimer', Cotizacion::where('empresa',auth()->user()->empresa)->get()->last()->created_at);
-        $sw = 1;
-
-    //Recorremos la sesion para obtener la fecha
-        foreach (Session::get('posttimer') as $key) {
-          if ($sw == 1) {
-            $ultimoingreso = $key;
-            $sw=0;
-        }
-    }
-
-//Tomamos la diferencia entre la hora exacta acutal y hacemos una diferencia con la ultima creación
-    $diasDiferencia = Carbon::now()->diffInseconds($ultimoingreso);
-
-//Si el tiempo es de menos de 30 segundos mandamos al listado general
-    if ($diasDiferencia <= 10) {
-      $mensaje = "El formulario ya ha sido enviado.";
-      return redirect('empresa/cotizaciones')->with('success', $mensaje);
-  }
-        }
-
         $nro=Numeracion::where('empresa',Auth::user()->empresa)->first();
         $caja=$nro->cotizacion;
         while (true) {
