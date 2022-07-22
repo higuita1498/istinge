8d5b5848a2870868740bfc4eb3682777a67610af
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-07-22 16:38:38 -0500

    ok

--- app/Services/ElectronicBillingService.php
+++ app/Services/ElectronicBillingService.php
@@ -19,7 +19,7 @@ class ElectronicBillingService
     {
         $this->baseUri = "https://apine.efacturacadena.com";
         $this->authorizationToken = "42e5b496-d882-4041-97ec-e3e91750805f990ef12f-36ff-454b-b020-fb19e953c37397478011-edaf-4f66-9945-81b691a718b118213614-da33-4e7b-9c22-ce78e0d55cf0";
-        $this->partnershipId = 1128464945;
+        $this->partnershipId = "1128464945";
     }
 
 
