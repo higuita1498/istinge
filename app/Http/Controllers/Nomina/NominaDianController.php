9481c10abfb3fba1efd12cfca8bfd30e0cbbd66a
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-08-11 16:04:39 -0500

    nominaservices error

--- app/Http/Controllers/Nomina/NominaDianController.php
+++ app/Http/Controllers/Nomina/NominaDianController.php
@@ -952,7 +952,7 @@ class NominaDianController extends Controller
             "CodigoQR" => "",
             "InformacionGeneral" => [
                 "Version" => "V1.0: Documento Soporte de Pago de Nómina Electrónica",
-                "Ambiente" => 1,
+                "Ambiente" => "1",
                 "TipoXML" => $nomina->emitida == 4 ? "103" : "102",
                 "FechaGen" => date('Y-m-d'),
                 "HoraGen" => date('H:i:sP'),
@@ -1066,7 +1066,7 @@ class NominaDianController extends Controller
                 ],
                 "InformacionGeneral" => [
                     "Version" => "V1.0: Nota de Ajuste de Documento Soporte de Pago de Nómina Electrónica",
-                    "Ambiente" => 1,
+                    "Ambiente" => "1",
                     "TipoXML" => "103",
                     "CUNE" => $cuneEliminar,
                     "EncripCUNE" => "CUNE-SHA384",
