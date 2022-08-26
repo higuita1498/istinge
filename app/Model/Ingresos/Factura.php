9716eb911e9994ccaa7ba68aaac0adb99786ebb9
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-08-26 17:30:31 -0500

    correcion factura crc

--- app/Model/Ingresos/Factura.php
+++ app/Model/Ingresos/Factura.php
@@ -830,7 +830,7 @@ public function forma_pago()
         
         /*>>>>>>>>>>>>>>>>>>>>>>>>>> Saldo mes Actual <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
         
-        $facturasActuales = Factura::where('cliente',$this->cliente)->where('vencimiento','>',$fechaActual)->get();
+        $facturasActuales = Factura::where('cliente',$this->cliente)->where('vencimiento','>',$fechaActual)->where('estatus','!=',2)->get();
         
         //sumamos todo lo que deba el cliente despues de la fecha de vencimiento
         foreach($facturasActuales as $actual){
@@ -853,7 +853,7 @@ public function forma_pago()
         /*>>>>>>>>>>>>>>>>>>>>>>>>>> Saldo mes Anterior <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
         //traemos todas las facturas que el vencimiento haya pasado la fecha actual.
-        $facturasVencidas = Factura::where('cliente',$this->cliente)->where('vencimiento','<',$fechaActual)->get();
+        $facturasVencidas = Factura::where('cliente',$this->cliente)->where('vencimiento','<',$fechaActual)->where('estatus','!=',2)->get();
 
         //sumamos todo lo que deba el cliente despues de la fecha de vencimiento
         foreach($facturasVencidas as $vencida){
@@ -864,7 +864,7 @@ public function forma_pago()
 
         /*>>>>>>>>>>>>>>>>>>>>>>>>>> Saldo mes Actual <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
-        $facturasActuales = Factura::where('cliente',$this->cliente)->where('vencimiento','>',$fechaActual)->get();
+        $facturasActuales = Factura::where('cliente',$this->cliente)->where('vencimiento','>',$fechaActual)->where('estatus','!=',2)->get();
 
         //sumamos todo lo que deba el cliente despues de la fecha de vencimiento
         foreach($facturasActuales as $actual){
