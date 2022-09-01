9191c1e903ade76f98d999fddeca601499fe750d
higuita1498 <31171113+higuita1498@users.noreply.github.com>
2022-09-01 10:47:04 -0500

    correcciones prorrateo

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('/import_bmus','Controller@import_bmus')->name('import_bmus');
 Route::get('/import_mikrotiks','Controller@import_mikrotiks')->name('import_mikrotiks');
 Route::get('/generarfactura','CronController@CrearFactura')->name('CrearFactura');
 Route::get('/deleteAll','CronController@deleteFactura');
+Route::get('/aplicateProrrateo','CronController@aplicateProrrateo');
 Route::get('/cortarfacturas','CronController@CortarFacturas')->name('CortarFacturas');
 Route::get('/enviarsms','CronController@EnviarSMS')->name('EnviarSMS');
 Route::get('/migrarCRM','CronController@migrarCRM')->name('migrarCRM');
