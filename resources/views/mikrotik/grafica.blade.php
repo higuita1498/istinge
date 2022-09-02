26ca2f7173e5493d9ae15c9bf2da37eb0dde5d44
AJ23ThemeMaster <symbianvenezuela@gmail.com>
2022-09-02 17:34:16 -0400

    Grafica MK

--- resources/views/mikrotik/grafica.blade.php
+++ resources/views/mikrotik/grafica.blade.php
@@ -56,11 +56,20 @@
     			</table>
     		</div>
     	</div>
-    	
+
     	<div class="col-md-12">
-    	    <input type="hidden" id="type_interface" name="type_interface" value="">
-    	    <input type="hidden" id="interface" name="interface" value="">
-    	    <input type="hidden" id="id" name="id" value="{{$mikrotik->id}}">
+    		<div class="row text-center">
+		        <input type="hidden" id="id" name="id" value="{{$mikrotik->id}}">
+		    	<div class="col-md-4 form-group mt-5 offset-md-4">
+		            <div class="input-group">
+		                <select class="form-control selectpicker" name="interfaz" id="interfaz" required="" title="Seleccione la Interfaz" data-live-search="true" data-size="5">
+		                </select>
+		            </div>
+		        </div>
+	        </div>
+        </div>
+
+    	<div class="col-md-12 d-none" id="div_grafica">
     	    <div id="container" style="min-width: 400px; height: 400px; margin: 30px auto"></div>
     	    <center><div id="trafico" style="font-weight: 600;font-family: Signika, serif;font-size: 1.5em;"></div></center>
     	</div>
@@ -75,32 +84,43 @@
     	function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
     
     	var chart;
-    	function requestDatta(interface,type_interface) {
-    	    var id = $("#id").val();
-    		$.ajax({
-    			url: 'graficajson',
-    			datatype: "json",
-    			success: function(data) {
-    				var midata = JSON.parse(data);
-    				if( midata.length > 0 ) {
-    					var RX=parseInt(midata[0].data);
-    					var TX=parseInt(midata[1].data);
-    					var x = (new Date()).getTime(); 
-    					shift=chart.series[0].data.length > 19;
-    					chart.series[0].addPoint([x, TX], true, shift);
-    					chart.series[1].addPoint([x, RX], true, shift);
-    					document.getElementById("trafico").innerHTML=formatBytes(TX) + " / " + formatBytes(RX);
-    				}else{
-    					document.getElementById("trafico").innerHTML="- / -";
-    				}
-    			},
-    			error: function(XMLHttpRequest, textStatus, errorThrown) { 
-    				console.error("Status: " + textStatus + " request: " + XMLHttpRequest); console.error("Error: " + errorThrown); 
-    			}       
-    		});
+
+    	function requestDatta(interfaz) {
+    	    if(interfaz){
+	    		$.ajax({
+	    			url: 'graficajson',
+	    			datatype: "json",
+	    			success: function(data) {
+	    				var midata = JSON.parse(data);
+	    				$("#div_grafica").removeClass('d-none');
+	    				if( midata.length > 0 ) {
+	    					var RX=parseInt(midata[0].data);
+	    					var TX=parseInt(midata[1].data);
+	    					var x = (new Date()).getTime();
+	    					shift=chart.series[0].data.length > 19;
+	    					chart.series[0].addPoint([x, TX], true, shift);
+	    					chart.series[1].addPoint([x, RX], true, shift);
+	    					document.getElementById("trafico").innerHTML=formatBytes(TX) + " / " + formatBytes(RX);
+	    				}else{
+	    					document.getElementById("trafico").innerHTML="- / -";
+	    				}
+	    			},
+	    			error: function(XMLHttpRequest, textStatus, errorThrown) {
+	    				$("#div_grafica").addClass('d-none');
+	    				Swal.fire({
+		                    type: 'error',
+		                    title: errorThrown,
+		                    html: '',
+		                })
+		                $('#interfaz').val('').selectpicker('refresh');
+	    				console.error("Status: " + textStatus + " request: " + XMLHttpRequest); console.error("Error: " + errorThrown);
+	    			}
+	    		});
+    		}
     	}	
     
     	$(document).ready(function() {
+    		getInterfaces({{$mikrotik->id}});
 			Highcharts.createElement('link', {
 			    href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
 			    rel: 'stylesheet',
@@ -207,7 +227,7 @@
 				events: {
 					load: function () {
 						setInterval(function () {
-							requestDatta(document.getElementById("interface").value,document.getElementById("type_interface").value);
+							requestDatta(document.getElementById("interfaz").value,);
 						}, 1000);
 					}				
 				}
