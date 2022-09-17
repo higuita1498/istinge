e093eb6accdd8d03128e7eeacba2a0109cfc24fb
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2022-09-17 11:32:28 -0500

    Update acciones.blade.php

--- resources/views/etiquetas/acciones.blade.php
+++ resources/views/etiquetas/acciones.blade.php
@@ -1,2 +1,3 @@
 <a href="javascript:editEtiqueta({{ $etiqueta->id}}, '{{ $etiqueta->nombre }}', '{{ $etiqueta->color}}')">editar</a>
+ | 
 <a href="javascript:destroyEtiqueta({{ $etiqueta->id}}, {{$etiqueta->radicados->count()}}, false)">eliminar</a>
