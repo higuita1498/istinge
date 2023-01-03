717766d3a164b32587a999f89dc4ff910294c9fa
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2023-01-02 20:25:36 -0500

    Update AsignacionesController.php

--- app/Http/Controllers/AsignacionesController.php
+++ app/Http/Controllers/AsignacionesController.php
@@ -1189,12 +1189,12 @@ class AsignacionesController extends Controller
     public function imprimir($id){
         $contrato = Contacto::where('id',$id)->where('empresa', Auth::user()->empresa)->first();
         $idContrato = request()->idContrato;
-        if($contrato) {
+        
             view()->share(['title' => 'Contrato de Internet']);
             //return view('pdf.contrato', compact('contrato', 'idContrato'));
             $pdf = PDF::loadView('pdf.contrato', compact('contrato', 'idContrato'));
             return  response ($pdf->stream())->withHeaders(['Content-Type' =>'application/pdf',]);
-        }
+        
     }
 
     public function show_campos_asignacion(){
