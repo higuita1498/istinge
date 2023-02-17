d8b4327986538c749c969f12f941c653b0ba067e
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2023-02-17 12:51:28 -0500

    Update PqrsController.php

--- app/Http/Controllers/PqrsController.php
+++ app/Http/Controllers/PqrsController.php
@@ -144,7 +144,8 @@ class PqrsController extends Controller
                 );
                 config(['mail'=>$new]);
             }
-    
+            
+            self::sendInBlue($correo->build()->render(), $correo->subject, [$pqrs->email], $correo->name, []);
             // Mail::to($pqrs->email)->send($correo);
             
             return redirect('empresa/pqrs')->with('success', 'Se ha registrado la respuesta al PQRS satisfactoriamente.');
