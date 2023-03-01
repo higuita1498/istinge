2f5cb611c20f42fd33529c1e7656ae569e4ead06
SantiagoSierra <48597889+SantiagoSierra@users.noreply.github.com>
2023-03-01 16:17:11 -0500

    boton crm +

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -3581,4 +3581,26 @@ class ContratosController extends Controller
        }
     
 
+
+       public function forzarCrm($idContrato){
+
+        $contrato = Contrato::find($idContrato);
+
+        //crm registro
+        $crm = new CRM();
+        $crm->cliente = $contrato->cliente()->id;
+        $crm->servidor = isset($contrato->server_configuration_id) ? $contrato->server_configuration_id : '';
+        $crm->grupo_corte = isset($contrato->grupo_corte) ? $contrato->grupo_corte : '';
+        $crm->estado = 0;
+        if($lastFact = $contrato->lastFactura()){
+            $crm->factura = $lastFact->id;
+        }
+        $crm->save();
+
+
+        return back()->with('success', 'Se genero un registro CRM en la cartera');
+
+       }
+
+
 }
