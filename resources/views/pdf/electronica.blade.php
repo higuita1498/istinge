661073c846427038dce9e91d82c8badd942e43e3
higuita1498 <higuitajokers@gmail.com>
2023-06-28 09:46:24 -0500

    ok

--- resources/views/pdf/electronica.blade.php
+++ resources/views/pdf/electronica.blade.php
@@ -626,7 +626,8 @@
                 </tr>
                 <tr class="tr-estadocuenta-precio">
                 <td><li>{{$empresa->moneda}} {{App\Funcion::Parsear($factura->estadoCuenta()->saldoMesAnterior)}}</li></td>
-                <td><li>{{$empresa->moneda}} {{App\Funcion::Parsear($factura->estadoCuenta()->saldoMesActual)}}</li></td>
+                {{-- <td><li>{{$empresa->moneda}} {{App\Funcion::Parsear($factura->estadoCuenta()->saldoMesActual)}}</li></td> --}}
+                <td><li>$0</li></td>
                 <td><li>{{$empresa->moneda}} 0</li></td>
                 <td><li>{{$empresa->moneda}} 0</li></td>
                 <td><li>{{$empresa->moneda}} 0</li></td>
