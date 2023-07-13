c8d0b87295327ab70b990bacdc9a45c210ee6a43
juanjo1129 <jjtuiranpadilla@gmail.com>
2023-07-13 17:27:38 -0500

    modified:   app/Http/Controllers/CRMController.php 	new file:   public/uniqueid

--- app/Http/Controllers/CRMController.php
+++ app/Http/Controllers/CRMController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
 use Mail; 
 use Validator;
