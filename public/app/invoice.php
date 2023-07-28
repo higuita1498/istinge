17d8e993a566801e624f41886fb1c44a4ba5ca93
Reywis Veliz <reywisveliz@gmail.com>
2023-07-28 10:51:28 -0500

    subiendo algunas mejoras para la integracion de la pasarela

--- public/app/invoice.php
+++ public/app/invoice.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
   <!--favicon-->
   <link rel="icon" href="assets/images/logo0.png" type="image/x-icon">
   <!-- Vector CSS -->
-  
+
   <!-- simplebar CSS-->
   <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
   <!-- Bootstrap core CSS-->
@@ -42,7 +42,7 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
   <link href="assets/css/sidebar-menu.css" rel="stylesheet"/>
   <!-- Custom Style-->
   <link href="assets/css/app-style.css" rel="stylesheet"/>
-  
+
   <link href="../software/vendors/sweetalert2/sweetalert2.min.css" rel="stylesheet"/>
   <style>.invoice-title h2, .invoice-title h3 {display: inline-block; }.table > tbody > tr > .no-line { border-top: none; } .table > thead > tr > .no-line { border-bottom: none; }.table > tbody > tr > .thick-line { border-top: 2px solid; }</style>
     <style type="text/css">
@@ -204,14 +204,14 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
 </head>
 
 <body class="bg-theme bg-theme2">
- 
+
 <!-- Start wrapper-->
  <div id="wrapper">
- 
+
   <?php include "partials/header.php"; ?>
 
 <div class="clearfix"></div>
-  
+
   <div class="content-wrapper">
    <!-- <div class="card mt-3" style="border-radius: 25px;">
     <div class="card-content" style="background-color: white; border-radius: 25px;"><br>
@@ -304,7 +304,7 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
                                         <input type="hidden" name="reference" id="reference" value="REFERENCIA_DE_PAGO" />
                                         <!-- OPCIONALES -->
                                         <input type="hidden" name="redirect-url" value="https://istingenieria.online/app/wompi.php" />
-                                        <button class="btn btn-success" type="submit">Pagar con Wompi</button>
+                                        <button class="btn btn-success" type="submit">Pagar con Wompi 1</button>
                                     </form>
                                 </div>
                 			</div>
@@ -321,27 +321,27 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
                 	</div>
                 </div>
             </div>
-        </div> 
+        </div>
 
       <!--End Dashboard Content-->
-    
+
   <!--start overlay-->
       <div class="overlay toggle-menu"></div>
     <!--end overlay-->
-    
+
     </div>
     <!-- End container-fluid-->
-    
+
     </div><!--End content-wrapper-->
    <!--Start Back To Top Button-->
     <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
     <!--End Back To Top Button-->
   <!--Start footer-->
   <!--End footer-->
-  
+
   <!--start color switcher-->
   <!--end color switcher-->
-   
+
   </div><!--End wrapper-->
 
   <!-- Bootstrap core JavaScript-->
@@ -360,7 +360,7 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
                         $('#invoice_error').html('');
                         document.getElementById('cliente_nombre').innerHTML = data.cliente.nombre;
                         document.getElementById('cliente_nit').innerHTML = data.cliente.nit;
-                        
+
                         document.getElementById('factura_codigo').innerHTML = data.factura.codigo;
                         document.getElementById('factura_fecha').innerHTML = data.factura.fecha;
                         document.getElementById('factura_vencimiento').innerHTML = data.factura.vencimiento;
@@ -371,18 +371,18 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
                         var monto_pago = parseFloat(data.factura.precio)+parseFloat(0);
                         $('#amount-in-cents').val(monto_pago+'00');
                         $('#reference').val(data.factura.codigo);
-                        
+
                         if(data.factura.desc == null){
                             document.getElementById('factura_desc').innerHTML = '0.00';
                         }else{
                             document.getElementById('factura_desc').innerHTML = data.factura.desc;
                         }
-                        
+
                         document.getElementById('factura_impuesto').innerHTML = data.factura.nombre;
                         document.getElementById('factura_porcentaje').innerHTML = data.factura.porcentaje;
                         document.getElementById('factura_subtotal').innerHTML = parseFloat(data.factura.precio).toFixed(2);
                         document.getElementById('factura_total').innerHTML = parseFloat(data.factura.precio).toFixed(2);
-                        
+
                         $("#buyerEmail").val(data.cliente.email);
                         $("#buyerFullName").val(data.cliente.nombre);
                         $("#referenceCode").val(data.factura.codigo);
@@ -396,20 +396,20 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
             });
             return false;
         });
-        
+
         function signature(){
             var MD5 = function(d){result = M(V(Y(X(d),8*d.length)));return result.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_};
             var firma_cadena = '538AwCyXSvTiqPE5F1xsj91F6y~899988~'+$("#referenceCode").val()+'~'+$("#amount").val()+'~COP';
             $("#signature").val(MD5(firma_cadena));
         }
-        
+
         function confirmarAlert(form, mensaje="Será redireccionado a la pasarela de pago PayU", submensaje='¿Desea continuar?', confirmar='Si'){
         /*if($("#buyerFullName").val() != '' && $("#buyerEmail").val() != ''){*/
         if($("#amount").val() != '' && $("#referenceCode").val() != '' ){
           signature();
           $("#btn-payu").click();
           cargando(true);
-          
+
           /*swal({
             title: mensaje,
             text: submensaje,
@@ -439,26 +439,26 @@ if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
         }
       }
     </script>
-   
+
   <script src="assets/js/popper.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
-  
+
  <!-- simplebar js -->
   <script src="assets/plugins/simplebar/js/simplebar.js"></script>
   <!-- sidebar-menu js -->
   <script src="assets/js/sidebar-menu.js"></script>
   <!-- loader scripts -->
-  
+
   <!-- Custom scripts -->
   <script src="assets/js/app-script.js"></script>
   <!-- Chart js -->
-  
-  
- 
+
+
+
   <!-- Index js -->
   <?php include "partials/whatsapp.php"; ?>
   <script src="../software/vendors/sweetalert2/sweetalert2.min.js"></script>
-  
+
 
 </body>
 </html>
--- public/pay.php
+++ public/pay.php
@@ -20,7 +20,7 @@
     <link href="./css/aos.min.css" rel="stylesheet">
     <link href="./css/swiper.css" rel="stylesheet">
     <link href="./css/style.css" rel="stylesheet">
-    
+
     <link rel="icon" href="./assets/images/favicon.png">
     <style>
         .loader {
@@ -139,7 +139,7 @@
 						</div>
 					</div>
 				</div>
-				
+
 				<div class="row" style="display: none;" id="mensaje">
 				    <div class="col-lg-8 offset-lg-2">
 				        <div class="table-responsive">
@@ -181,7 +181,7 @@
                             </table>
                         </div>
                     </div>
-                    
+
                     <div class="col-12 mb-5 d-none">
                         <form action="https://checkout.wompi.co/p/" method="GET" id="form-wompi">
                             <input type="hidden" name="public-key" value="pub_prod_7BEvWOpu64Zl6OKx3ZP9p1XGBn5eq7bG" />
@@ -192,31 +192,59 @@
                             <button class="btn btn-main" style="color: var(--secondary);" type="submit">Pagar con Wompi</button>
                         </form>
                     </div>
-                    
+
                     <div class="col-12 mb-5 d-none">
                         <form method="post" action="https://checkout.payulatam.com/ppp-web-gateway-payu/" id="form-payu">
                             <input name="ApiKey" id="ApiKey" type="hidden" value="538AwCyXSvTiqPE5F1xsj91F6y">
                             <input name="merchantId"    type="hidden"  value="899988"   >
                             <input name="accountId"     type="hidden"  value="906617" >
-                            
+
                             <input name="buyerEmail" id="buyerEmail" type="hidden">
                             <input name="buyerFullName" id="buyerFullName" type="hidden">
-                            
+
                             <input name="referenceCode" id="referenceCode" type="hidden">
                             <input name="currency" id="currency" type="hidden" value="COP">
                             <input name="amount" id="amount" type="hidden">
                             <input name="description" id="description" type="hidden">
                             <input name="test" type="hidden" value="0" >
                             <input name="tax" type="hidden" value="0" >
-                            
+
                             <input name="signature" id="signature" type="hidden">
-                            
+
                             <input name="responseUrl" type="hidden" value="https://toplink.com.co/pay.php">
                             <input name="confirmationUrl" type="hidden" value="https://toplink.com.co/pay.php">
                             <input name="Submit" type="submit"  value="Enviar">
                         </form>
                     </div>
-                    
+
+                    <!-- INTEGRANDO LA PASARELA DE PAGO TOPPAY -->
+
+                    <div class="col-12 mb-5 d-none">
+                        <form method="post" action="https://production.toppaylatam.com/api/transactions" id="form-toppay">
+                            <input name="ApiKey" id="ApiKey" type="hidden" value="5eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJ">
+                            <!-- <input name="merchantId"    type="hidden"  value="899988"   >
+                            <input name="accountId"     type="hidden"  value="906617" > -->
+
+                            <input name="buyerEmail" id="buyerEmail" type="hidden">
+                            <input name="buyerFullName" id="buyerFullName" type="hidden">
+
+                            <input name="referenceCode" id="referenceCode" type="hidden">
+                            <input name="expiration" id="expirationy" type="hidden" value="2023-07-26">
+                            <input name="currency" id="currency" type="hidden" value="COP">
+                            <input name="amount" id="amount" type="hidden">
+                            <input name="description" id="description" type="hidden">
+                            <input name="test" type="hidden" value="0" >
+                            <input name="tax" type="hidden" value="0" >
+
+                            <input name="signature" id="signature" type="hidden">
+
+                            <input name="responseUrl" type="hidden" value="https://toplink.com.co/pay.php">
+                            <input name="confirmationUrl" type="hidden" value="https://toplink.com.co/pay.php">
+                            <input name="Submit" type="submit"  value="Enviar">
+                        </form>
+                    </div>
+                    <!-- FIN DE LA INTEGRACION  -->
+
                     <div class="col-12" style="text-align:center;">
                         <div class="contact-form">
                             <button class="btn btn-main" style="color: var(--secondary);" type="submit" onclick="confirmar('form-wompi');">Pagar con Wompi</button>
@@ -226,7 +254,7 @@
                 </div>
         </div> <!-- end of container -->
     </div>
-    
+
     <?php include('include/footer.php'); ?>
 
     <!-- Scripts -->
@@ -237,9 +265,9 @@
     <script src="./js/aos.js"></script><!-- AOS on Animation Scroll -->
     <script src="./js/script.js"></script>  <!-- Custom scripts -->
     <script src="./software/vendors/sweetalert2/sweetalert2.min.js"></script>
-    
+
     <script>
-        	
+
         //function confirmar(form, mensaje="El uso de la plataforma WOMPI tiene un valor de 2800 COP adicional al monto de la facturación", submensaje='¿Desea continuar?', confirmar='Si'){
         function confirmar(form, mensaje="Será redireccionado a la pasarela de pago WOMPI", submensaje='¿Desea continuar?', confirmar='Si'){
             if($("#buyerFullName").val() != ''){
@@ -270,7 +298,7 @@
                 })
             }
         }
-            
+
         function consultar(){
             if ($("#nombre").val() == '' || $("#identificacion").val() == '') {
                 Swal.fire({
@@ -284,7 +312,7 @@
                 });
                 return false;
             }
-            
+
             $.ajax({
                 url: '/software/factura/'+$("#identificacion").val(),
                 beforeSend: function(){
@@ -309,7 +337,7 @@
                         $("#resul_vencimiento").text(data.vencimiento);
                         $("#resul_price").text(parseFloat(data.price)+parseFloat(0)+' COP');
                         $("#description").val(data.plan);
-                        
+
                         $("#buyerEmail").val(data.email);
                         $("#buyerFullName").val(data.nombre);
                         $("#referenceCode").val(data.factura);
@@ -344,7 +372,7 @@
                 }
             });
         }
-            
+
         function cargando(abierta){
             if (abierta) {
                 $(".loader").show();
@@ -352,18 +380,18 @@
                 $(".loader").hide();
             }
         }
-        
+
         function signature(){
             var MD5 = function(d){result = M(V(Y(X(d),8*d.length)));return result.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_};
             var firma_cadena = '538AwCyXSvTiqPE5F1xsj91F6y~899988~'+$("#referenceCode").val()+'~'+$("#amount").val()+'~COP';
             $("#signature").val(MD5(firma_cadena));
         }
-        
+
         function confirmarAlert(form, mensaje="Será redireccionado a la pasarela de pago PayU", submensaje='¿Desea continuar?', confirmar='Si'){
         /*if($("#buyerFullName").val() != '' && $("#buyerEmail").val() != ''){*/
         if($("#amount").val() != '' && $("#referenceCode").val() != '' ){
           signature();
-          
+
           swal({
             title: mensaje,
             text: submensaje,
