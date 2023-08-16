af5e490b014a4d07d9d450e1eef42956d1e82567
higuita1498 <higuitajokers@gmail.com>
2023-08-16 13:04:50 -0500

    ok

--- resources/views/reportes/cajas/index.blade.php
+++ resources/views/reportes/cajas/index.blade.php
@@ -114,7 +114,7 @@
                         </td>
                         <td>
 
-                            {{ $movimiento->padre()->created_by() ? $movimiento->padre()->created_by()->nombres : ''}}
+                            {{ isset($movimiento->padre() && $movimiento->padre()->created_by())  ? $movimiento->padre()->created_by()->nombres : ''}}
                         </td>
                         
                         <td>
