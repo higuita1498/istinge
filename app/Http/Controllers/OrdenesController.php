b70d1605defaf4eec07661bd1b7097c7f830aeec
higuita1498 <higuitajokers@gmail.com>
2023-08-29 14:22:20 -0500

    ok

--- app/Http/Controllers/OrdenesController.php
+++ app/Http/Controllers/OrdenesController.php
@@ -144,29 +144,7 @@ class OrdenesController extends Controller
   * @return redirect
   */
   public function store(Request $request){
-      
-      if( Ordenes_Compra::where('empresa',auth()->user()->empresa)->count() > 0){
-      //Tomamos el tiempo en el que se crea el registro
-    Session::put('posttimer', Ordenes_Compra::where('empresa',auth()->user()->empresa)->get()->last()->created_at);
-    $sw = 1;
-
-    //Recorremos la sesion para obtener la fecha
-    foreach (Session::get('posttimer') as $key) {
-      if ($sw == 1) {
-        $ultimoingreso = $key;
-        $sw=0;
-      }
-    }
-
-//Tomamos la diferencia entre la hora exacta acutal y hacemos una diferencia con la ultima creación
-    $diasDiferencia = Carbon::now()->diffInseconds($ultimoingreso);
-
-//Si el tiempo es de menos de 30 segundos mandamos al listado general
-    if ($diasDiferencia <= 10) {
-      $mensaje = "El formulario ya ha sido enviado.";
-     return redirect('empresa/ordenes')->with('success', $mensaje);
-    }
-      }
+    
       
     $nro=Numeracion::where('empresa',Auth::user()->empresa)->first();
     $caja=$nro->orden;
