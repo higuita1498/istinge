b5599157139cf43369a04fc260af730fe7252dd0
juanjo1129 <reywisveliz@gmail.com>
2023-09-06 12:51:52 -0500

    solucionado el error de subida masiva de contratos excel

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -3173,8 +3173,8 @@ class ContratosController extends Controller
             }
             if($request->plan != ""){
                 $miko = Mikrotik::where('nombre', $request->mikrotik)->first();
-                dd($miko);
                 if(PlanesVelocidad::where('name', $request->plan)->where('mikrotik', $miko->id)->count() == 0){
+                    dd("ingreso en el si para validar planes");
                     $error->plan = "El plan de velocidad ".$request->plan." ingresado no se encuentra en nuestra base de datos";
                 }
             }
