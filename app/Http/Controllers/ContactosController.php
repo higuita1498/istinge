a6ca32482ea417ba32e88e7604d6daf72bf7853f
juanjo1129 <reywisveliz@gmail.com>
2023-09-20 17:36:18 -0500

    ok

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -230,7 +230,7 @@ class ContactosController extends Controller
         $contactos = Contacto::where('empresa', Auth::user()->empresa)->get();
         $tipo_usuario = 0;
         $tabla = Campos::join('campos_usuarios', 'campos_usuarios.id_campo', '=', 'campos.id')->where('campos_usuarios.id_modulo', 1)->where('campos_usuarios.id_usuario', Auth::user()->id)->where('campos_usuarios.estado', 1)->orderBy('campos_usuarios.orden', 'ASC')->get();
-        dd($contactos[0]);
+        dd($tabla);
         view()->share(['invert' => true]);
 
         return view('contactos.indexnew')->with(compact('contactos', 'totalContactos', 'tipo_usuario', 'tabla'));
