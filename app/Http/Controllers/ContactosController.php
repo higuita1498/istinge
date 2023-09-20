c90b5dd28cab0db983a6fc3b6bce37f2d426cde7
juanjo1129 <reywisveliz@gmail.com>
2023-09-20 16:52:20 -0500

    realizando configuracion para hacer visible el municipio en el cliente

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -216,7 +216,7 @@ class ContactosController extends Controller
 
     public function clientes(Request $request)
     {
-
+        dd($request);
         $this->getAllPermissions(Auth::user()->id);
         view()->share(['title' => 'Clientes', 'subseccion' => 'clientes']);
         $busqueda = false;
