f77af045093e72e67d7b97ed0fb39fc7b2082808
juanjo1129 <reywisveliz@gmail.com>
2023-09-22 13:05:06 -0500

    ok

--- resources/views/contactos/indexnew.blade.php
+++ resources/views/contactos/indexnew.blade.php
@@ -182,7 +182,7 @@
 			},
 			columns: [
 			    @foreach($tabla as $campo)
-                {{$campo}}
+                {{'$campo->campo'}}
 			        @if($tipo_usuario == 1)
 			            @if($campo->campo != 'contrato')
 			                {data: '{{$campo->campo}}'},
