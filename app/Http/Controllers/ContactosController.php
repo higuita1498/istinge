0afbdf3f1c52b65db28ba2ac3f1b77c91512536a
juanjo1129 <reywisveliz@gmail.com>
2023-09-22 12:46:40 -0500

    ok

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -227,7 +227,8 @@ class ContactosController extends Controller
         $tipos_empresa = TipoEmpresa::where('empresa', Auth::user()->empresa)->get();
         // $contactos = $this->busqueda($request, [0, 2]);
         $totalContactos = Contacto::where('empresa', Auth::user()->empresa)->count();
-        $contactos = Contacto::where('empresa', Auth::user()->empresa)->get();
+        // $contactos = Contacto::where('empresa', Auth::user()->empresa)->get();
+        $contactos = DB::table('contactos')->join('municipios', 'contactos.fk_idmunicipio', '=', 'municipios.id')->select('contactos.*', 'municipios.nombre as nombre_municipio')->get();
         $tipo_usuario = 0;
         $tabla = Campos::join('campos_usuarios', 'campos_usuarios.id_campo', '=', 'campos.id')->where('campos_usuarios.id_modulo', 1)->where('campos_usuarios.id_usuario', Auth::user()->id)->where('campos_usuarios.estado', 1)->orderBy('campos_usuarios.orden', 'ASC')->get();
 
@@ -257,7 +258,7 @@ class ContactosController extends Controller
 
     public function busqueda($request, $tipo = false)
     {
-        dd("ok");
+
         $this->getAllPermissions(Auth::user()->id);
         $campos = [
             'id',
