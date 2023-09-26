1dd17526775e76ad94f217cdde484471c8281912
juanjo1129 <reywisveliz@gmail.com>
2023-09-26 09:03:39 -0500

    viendo el error de importacion de contratos

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -3185,7 +3185,7 @@ class ContratosController extends Controller
                 } else {
                     // Manejar el caso en el que $miko no sea un objeto válido
                 }
-
+    dd($request->plan);
                 if(PlanesVelocidad::where('name', $request->plan)->where('mikrotik', $mikoId)->count() === 0){
                     $error->plan = "El plan de velocidad ".$request->plan." ingresado no se encuentra en nuestra base de datos";
                 }
