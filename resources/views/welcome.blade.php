dac99d854173f38f86fc9684c1e7e38b583c1e1a
juanjo1129 <reywisveliz@gmail.com>
2023-10-06 12:01:42 -0500

    subiendo datos de pago de factura

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
                             <label for="archivo">Adjuntar pago:</label>
                             <input type="file" name="archivo" id="archivo" class="form-control">
                         </div> --}}
-                        <div class="container" style="text-align: center;height: 100vh;line-height: 100vh;">
+                        <div class="container" style="text-align: center;height: 10vh;line-height: 10vh;">
                             <a href="https://wa.me/+573027188808" target="_blank" style="text-align: center;color:white;background:black;padding:10px;border-radius:5px;">¡Enviar comprobante!
                             {{-- <button>¡Enviar comprobante!</button> --}}
                             </a>
