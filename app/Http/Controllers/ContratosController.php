a191913fa4a2a1ba45d5ba8b148be04a549f9511
juanjo1129 <reywisveliz@gmail.com>
2023-10-11 12:05:04 -0500

    envio de contrato a microtik masivamente

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2790,9 +2790,10 @@ class ContratosController extends Controller
         $succ = 0; $fail = 0; $registro = false; $contracts_fallidos = ''; $contracts_correctos = '';
 
         $contratos = explode(",", $contratos);
-        dd($contratos);
+
         for ($i=0; $i < count($contratos) ; $i++) {
             $contrato = Contrato::find($contratos[$i]);
+            dd($contrato);
             if ($contrato) {
                 if($contrato->mk==0){
                     $plan = PlanesVelocidad::where('id', $contrato->plan_id)->first();
