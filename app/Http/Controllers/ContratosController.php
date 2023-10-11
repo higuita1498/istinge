d3e777f6c9ef722681470260284b913dc1272f1f
juanjo1129 <reywisveliz@gmail.com>
2023-10-11 11:49:10 -0500

    envio de contrato a microtik masivamente

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2783,14 +2783,14 @@ class ContratosController extends Controller
         ]);
     }
 
-    public function enviar_mk_lote($contratos,$plan){
-        dd($contratos);
+    public function enviar_mk_lote($contratos){
+
         $this->getAllPermissions(Auth::user()->id);
 
         $succ = 0; $fail = 0; $registro = false; $contracts_fallidos = ''; $contracts_correctos = '';
 
         $contratos = explode(",", $contratos);
-
+        dd($contratos);
         for ($i=0; $i < count($contratos) ; $i++) {
             $contrato = Contrato::find($contratos[$i]);
             if ($contrato) {
