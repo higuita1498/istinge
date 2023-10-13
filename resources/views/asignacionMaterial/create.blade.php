ee8b7459508744b3b177c07bb465288e64f1abce
juanjo1129 <reywisveliz@gmail.com>
2023-10-13 09:30:54 -0500

    subiendo mejoras para el modulo de almacenar de herramientas para tecnicos

--- resources/views/asignacionMaterial/create.blade.php
+++ resources/views/asignacionMaterial/create.blade.php
@@ -75,7 +75,7 @@
   				<label class="col-sm-4 col-form-label">Tecnico <span class="text-danger">*</span></label>
 	  			<div class="col-sm-8">
             <div class="input-group">
-              <select class="form-control selectpicker" name="cliente" id="cliente" required="" title="Seleccione" data-live-search="true" data-size="5" onchange="toggleDiv(this.value);">
+              <select class="form-control selectpicker" name="cliente" id="cliente" required="" title="Seleccione" data-live-search="true" data-size="5" onchange="toggleDiv(this.value)">
                 @foreach($clientes as $client)
                   <option {{old('cliente')==$client->id?'selected':''}} {{$cliente==$client->id?'selected':''}}  value="{{$client->id}}">{{$client->nombres}}</option>
                 @endforeach
