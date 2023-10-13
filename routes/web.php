0f9409158cc4464972955b888dac3c3ed93825b0
juanjo1129 <reywisveliz@gmail.com>
2023-10-13 10:43:30 -0500

    subiendo mejoras para el modulo de almacenar de herramientas para tecnicos

--- routes/web.php
+++ routes/web.php
@@ -408,7 +408,7 @@ Route::group(['prefix' => 'empresa', 'middleware' => ['auth']], function() {
     // Rutas asignacion de material
     Route::get('asignacion_material', 'AsignacionMaterialController@index')->name('asignacionmaterial.index-electronica');
     Route::get('asignacion_material/create', 'AsignacionMaterialController@create')->name('asignacionmaterial.create');
-    Route::get('asignacion_material/store', 'AsignacionMaterialController@store')->name('asignacionmaterial.store');
+    Route::post('asignacion_material/store', 'AsignacionMaterialController@store')->name('asignacionmaterial.store');
 
 
 
