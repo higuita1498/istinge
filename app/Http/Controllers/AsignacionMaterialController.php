2c573e7fbb321e1782fc123f3048c87df71dee3b
juanjo1129 <reywisveliz@gmail.com>
2023-10-17 15:59:47 -0500

    terminando modulo de herramientas

--- app/Http/Controllers/AsignacionMaterialController.php
+++ app/Http/Controllers/AsignacionMaterialController.php
@@ -509,7 +509,7 @@ class AsignacionMaterialController extends Controller{
         $facturas = AsignarMaterial::query()
             ->join('usuarios as c', 'asignacion_materials.id_tecnico', '=', 'c.id')
             ->join('items_asignar_materials as if', 'asignacion_materials.id', '=', 'if.id_factura_materials')
-            ->select('asignacion_materials.id', 'asignacion_materials.correo','asignacion_materials.nro', DB::raw('c.nombres as nombrecliente'),DB::raw('c.email as emailcliente'),'asignacion_materials.cliente', 'asignacion_materials.fecha')
+            ->select('asignacion_materials.id', 'asignacion_materials.correo','asignacion_materials.nro', 'c.nombres as nombrecliente','c.email as emailcliente','asignacion_materials.cliente', 'asignacion_materials.fecha')
             ->groupBy('asignacion_materials.id');
 
         // if ($request->filtro == true) {
