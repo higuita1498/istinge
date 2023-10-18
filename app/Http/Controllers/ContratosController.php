50aac95ee316ec338033b5e82f8ba343b3a229dd
juanjo1129 <reywisveliz@gmail.com>
2023-10-18 11:03:54 -0500

    agrenado municipio en contratos

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -424,9 +424,7 @@ class ContratosController extends Controller
             ->editColumn('observaciones', function (Contrato $contrato) {
                 return ($contrato->observaciones) ? $contrato->observaciones : 'N/A';
             })
-            ->editColumn('fk_idmunicipio', function (Contrato $contrato) {
-                return $contrato->fk_idmunicipio;
-            })
+
             ->editColumn('acciones', $modoLectura ?  "" : "contratos.acciones")
             ->rawColumns(['nro', 'client_id', 'fk_idmunicipio', 'nit', 'telefono', 'email', 'barrio', 'plan', 'mac', 'ipformat', 'grupo_corte', 'state', 'pago', 'servicio', 'factura', 'servicio_tv', 'acciones', 'vendedor', 'canal', 'tecnologia', 'observaciones', 'created_at'])
             ->toJson();
