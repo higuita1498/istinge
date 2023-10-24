e36f57be08b5006766f09358bec254f2ff0da07c
juanjo1129 <reywisveliz@gmail.com>
2023-10-24 10:28:44 -0500

    verificando el error de contrato

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -128,9 +128,9 @@ class ContratosController extends Controller
         // return $contratos->get();
         if ($request->filtro == true) {
             if($request->cliente_id){
-                // dd($request->cliente_id);
+
                 $contratos->where(function ($query) use ($request) {
-                    $query->where('contracts.client_id', $request->cliente_id);
+                    $query->orWhere('contracts.client_id', $request->cliente_id);
                 });
             }
             if($request->plan){
