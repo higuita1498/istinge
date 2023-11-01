1d3d2183b878aafaa35cd36d618741e216d777af
juanjo1129 <reywisveliz@gmail.com>
2023-11-01 11:54:36 -0500

    subiendo mejoras editar contrato

--- resources/views/contratos/edit.blade.php
+++ resources/views/contratos/edit.blade.php
@@ -284,7 +284,7 @@
                             <div class="col-md-4 form-group">
                                 <label class="control-label" id="div_local_address">Segmento de IP</label>
                                   <div class="input-group">
-                                    <input type="hidden" id="segmento_bd" value="{{ $contrato->local_address }}">
+                                    <input type="hidden" id="segmento_bd" value="{{ old('segmento_bd') }}">
                                     <select class="form-control selectpicker" name="local_address" id="local_address" required="" title="Seleccione" data-live-search="true" data-size="5">
 
                                     </select>
