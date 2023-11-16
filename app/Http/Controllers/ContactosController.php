8229289124933ba76ac471feeaefb72682b0253f
juanjo1129 <reywisveliz@gmail.com>
2023-11-16 13:52:25 -0500

    continuando con las modificaciones de campos extras en contactos

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -732,11 +732,11 @@ class ContactosController extends Controller
         $contacto = Contacto::join('contracts as cs', 'contactos.id', '=', 'cs.client_id')->where('contactos.id', $id)->first();
         if (isset($contacto->plan_id)) {
             if ($contacto->plan_id) {
-                $contacto = DB::select("SELECT C.id, C.nombre, C.boton_emision ,C.apellido1, C.apellido2, C.nit, C.tip_iden, C.telefono1, C.celular, C.estrato, 
+                $contacto = DB::select("SELECT C.id, C.nombre, C.boton_emision ,C.apellido1, C.apellido2, C.nit, C.tip_iden, C.telefono1, C.celular, C.estrato,
                 C.saldo_favor, C.saldo_favor2 ,CS.public_id as contrato, CS.facturacion, I.id as plan,
-                 GC.fecha_corte, GC.fecha_suspension, CS.servicio_tv FROM contactos AS C INNER JOIN contracts AS CS ON (C.id = CS.client_id) 
-                 INNER JOIN planes_velocidad AS P ON (P.id = CS.plan_id) INNER JOIN inventario AS I ON (I.id = P.item)  
-                 INNER JOIN grupos_corte AS GC ON (GC.id = CS.grupo_corte) WHERE CS.status = '1' 
+                 GC.fecha_corte, GC.fecha_suspension, CS.servicio_tv FROM contactos AS C INNER JOIN contracts AS CS ON (C.id = CS.client_id)
+                 INNER JOIN planes_velocidad AS P ON (P.id = CS.plan_id) INNER JOIN inventario AS I ON (I.id = P.item)
+                 INNER JOIN grupos_corte AS GC ON (GC.id = CS.grupo_corte) WHERE CS.status = '1'
                  AND C.status = '1' AND  C.id = '".$id."'");
             }
         }
@@ -1495,7 +1495,7 @@ class ContactosController extends Controller
 
     //metodo para añadir mas campos al formulario de contacto
     public function indexcampos(){
-
+        dd("quieres agregar mas campos al formulario");
          $modoLectura = auth()->user()->modo_lectura();
          $this->getAllPermissions(Auth::user()->id);
          $identificaciones = TipoIdentificacion::all();
