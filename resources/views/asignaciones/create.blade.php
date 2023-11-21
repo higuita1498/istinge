535b7cc59b46b6244efe3a45f367aa960897ab42
juanjo1129 <reywisveliz@gmail.com>
2023-11-21 13:45:40 -0500

    subiendo eliminar archivos adjuntos de los radicados

--- resources/views/asignaciones/create.blade.php
+++ resources/views/asignaciones/create.blade.php
@@ -132,7 +132,7 @@
 
                 <div class="col-md-3 form-group">
                     <label class="control-label" id="div_campo_h">Adjuntar audio</label>
-                    <input type="file" class="form-control" name="archivo_de_audio" accept="audio/*" required id="adjunto_audio" name="adjunto_audio"  value="{{old('adjunto_audio')}}">
+                    <input type="file" class="form-control" name="archivo_de_audio" accept="audio/*" required id="adjunto_audio" name="adjunto_audio"  >
                     {{-- <input type="file" class="form-control"  id="adjunto" name="adjunto4" value="{{$radicado->adjunto}}" accept=".jpg, .jpeg, .png, .pdf, .JPG, .JPEG, .PNG, .PDF" required> --}}
                     <span style="color: red;">
                         <strong>{{ $errors->first('adjunto_audio') }}</strong>
