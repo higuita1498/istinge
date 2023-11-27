9773368937ea2a8c5df60f9de83ad7a75c45f2e7
juanjo1129 <reywisveliz@gmail.com>
2023-11-27 11:08:22 -0500

    agregando modificacion a asiganaciones cuando hay mas contratos

--- resources/views/asignaciones/create.blade.php
+++ resources/views/asignaciones/create.blade.php
@@ -333,6 +333,7 @@
 
         // Función para actualizar dinámicamente el segundo select con el resultado del contrato
         function updateContratosSelect(contratos) {
+            console.log(contratos);
             var selectContrato = document.getElementById('idCliente');
             selectContrato.innerHTML = ''; // Limpiar opciones existentes
 
