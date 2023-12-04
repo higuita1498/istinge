a46313d7d51e9bbd1bae3a6606adec0765fae040
juanjo1129 <reywisveliz@gmail.com>
2023-12-04 18:14:15 -0500

    modificando roles y permiso en el sistema

--- resources/views/facturas/indexnew.blade.php
+++ resources/views/facturas/indexnew.blade.php
@@ -10,9 +10,9 @@
 	    </div>
 	@else
         <a href="javascript:abrirFiltrador()" class="btn btn-info btn-sm my-1" id="boton-filtrar"><i class="fas fa-search"></i>Filtrar</a>
-        @if(isset($_SESSION['permisos']))
+        @if(isset($_SESSION['permisos']['42']))
             @php
-                echo '<p>' . htmlspecialchars($_SESSION['permisos']['42']) . '</p>';
+                echo '<p>' . htmlspecialchars($_SESSION['permisos']) . '</p>';
             @endphp
         <a href="{{route('facturas.create')}}" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Nueva Factura de Venta</a>
         @endif
