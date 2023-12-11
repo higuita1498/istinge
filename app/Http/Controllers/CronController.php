b10c124b20b97cf14ac5a325ccbbe369682a5f7e
juanjo1129 <reywisveliz@gmail.com>
2023-12-11 13:11:52 -0500

    agregando columna para el carcter de combopay

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -1739,7 +1739,7 @@ class CronController extends Controller
         $empresa = Empresa::find(1);
         if($request->transaction_state == 'payment_approved'){
 
-            $factura = Factura::where('codigo', substr($request->invoice_number, 1))->first();
+            $factura = Factura::where('codigo', substr($request->invoice_number, $empresa->caracter_combo_pay))->first();
 
             if($factura->estatus == 1){
 
