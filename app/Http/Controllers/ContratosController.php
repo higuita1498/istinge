548d89ec4345397394c4b27bb737d58f60bceb7c
juanjo1129 <reywisveliz@gmail.com>
2023-12-22 11:40:43 -0500

    subiendo mejoras a ventas externas

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2140,7 +2140,7 @@ class ContratosController extends Controller
                 ->setCellValue($letras[18].$i, $contrato->facturacion())
                 ->setCellValue($letras[19].$i, $contrato->costo_reconexion)
                 ->setCellValue($letras[20].$i, ucfirst($contrato->tipo_contrato))
-                ->setCellValue($letras[21].$i, ucfirst($contrato->tipo_contrato));;
+                ->setCellValue($letras[21].$i, $contrato->nombre_municipio);
             $i++;
         }
 
