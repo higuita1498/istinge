0785f822a7ba6be2e1b4749b6f647480f0422726
juanjo1129 <reywisveliz@gmail.com>
2024-01-15 09:54:43 -0500

    mejorando planes de velocidad

--- app/Http/Controllers/PlanesVelocidadController.php
+++ app/Http/Controllers/PlanesVelocidadController.php
@@ -127,7 +127,7 @@ class PlanesVelocidadController extends Controller
                 return '<span class="text-' . $plan->type(true) . '">' . $plan->type(). '</span>';
             })
             ->editColumn('mikrotik', function (PlanesVelocidad $plan) {
-                return "<a href=" . route('mikrotik.show', $plan->mikrotik()->id) . " target='_blank'>{$plan->mikrotik()->nombre}</div></a>";
+                return "<a href=" . route('mikrotik.show', $plan->mikrotik()->id) . " target='_blank'>{$plan->mikrotik()->nombre}</div></a>, <a href=" . route('mikrotik.show', $plan->mikrotik1()->id) . " target='_blank'>{$plan->mikrotik1()->nombre}</div></a>";
                 return ;
             })
             ->editColumn('status', function (PlanesVelocidad $plan) {
