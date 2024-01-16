715cbbf9b962c597d0ab0d20f5ec3367efdd880e
Reywis <reywisveliz@gmail.com>
2024-01-16 11:34:36 -0500

    verificando la subida de contratos

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -3353,6 +3353,7 @@ class ContratosController extends Controller
                 if(Mikrotik::where('nombre', $request->mikrotik)->count() == 0){
                     $error->mikrotik = "El mikrotik ingresado no se encuentra en nuestra base de datos";
                 }
+                dd($request->mikrotik);
                 $miko = Mikrotik::where('nombre', $request->mikrotik)->first();
                 $mikoId = $miko->id;
             }
