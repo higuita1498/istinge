dbe9a3e0b52e24f4a93e263dfd12ad75b1de7343
Reywis <reywisveliz@gmail.com>
2024-01-31 13:30:34 -0500

    subiendo firma de asognaciones

--- resources/views/contratos/edit.blade.php
+++ resources/views/contratos/edit.blade.php
@@ -382,21 +382,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-4 form-group {{$contrato->tecnologia==2?'':'d-none'}}">
-                                <label class="control-label d-none">Ip Receptora <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" name="ip_receptora" id="ip_receptora" value="{{ $contrato->ip_receptora}}">
-                                <span class="help-block error">
-                                    <strong>{{ $errors->first('ip_receptora') }}</strong>
-                                </span>
-                            </div>
 
-                            <div class="col-md-4 form-group {{$contrato->tecnologia==2?'':'d-none'}}">
-                                <label class="control-label d-none">Puerto Receptor <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" name="puerto_receptor" id="puerto_receptor" value="{{ $contrato->puerto_receptor}}">
-                                <span class="help-block error">
-                                    <strong>{{ $errors->first('puerto_receptor') }}</strong>
-                                </span>
-                            </div>
                             <div class="col-md-4 form-group">
                                 <label class="control-label d-none">Tipo de Tecnología <span class="text-danger">*</span></label>
                                 <select class="form-control selectpicker" id="tecnologia" name="tecnologia" required="" title="Seleccione">
@@ -407,7 +393,21 @@
                                     <strong>{{ $errors->first('tecnologia') }}</strong>
                                 </span>
                             </div>
+                            <div class="col-md-4 form-group {{$contrato->tecnologia==2?'':'d-none'}}">
+                                <label class="control-label">Ip Receptora <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" name="ip_receptora" id="ip_receptora" value="{{ $contrato->ip_receptora}}">
+                                <span class="help-block error">
+                                    <strong>{{ $errors->first('ip_receptora') }}</strong>
+                                </span>
+                            </div>
 
+                            <div class="col-md-4 form-group {{$contrato->tecnologia==2?'':'d-none'}}">
+                                <label class="control-label ">Puerto Receptor <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" name="puerto_receptor" id="puerto_receptor" value="{{ $contrato->puerto_receptor}}">
+                                <span class="help-block error">
+                                    <strong>{{ $errors->first('puerto_receptor') }}</strong>
+                                </span>
+                            </div>
                             <div class="col-md-4 form-group {{$contrato->tecnologia==2?'':'d-none'}}" id="div_ap">
                                 <label class="control-label">Access Point Asociado <span class="text-danger">*</span></label>
                                 <select class="form-control selectpicker" id="ap" name="ap" title="Seleccione" data-live-search="true" data-size="5">
