f8f9b0397dc709cf93e6bdb27e14dd1ac3adad69
Reywis <reywisveliz@gmail.com>
2024-01-31 18:41:34 -0500

    subiendo mejoras contrato

--- resources/views/contratos/create.blade.php
+++ resources/views/contratos/create.blade.php
@@ -378,7 +378,7 @@
                                 <input type="text" class="form-control" name="direccion_local_address" id="local_address" onkeypress="return event.charCode >= 48 && event.charCode <=57 || event.charCode==46 || event.charCode==47">
                             </div>
 
-                            <div class="col-md-4 form-group d-none" id="div_profile" >
+                            <div class="col-md-4 form-group d-none" id="profile" >
                                 <label class="control-label">Profile</label>
                                 <input type="text" class="form-control" name="profile" id="profile" >
                             </div>
