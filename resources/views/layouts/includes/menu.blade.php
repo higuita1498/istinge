e0637e2d3675e2c06ce978ddcf800ac2fc8c36a0
Reywis <reywisveliz@gmail.com>
2024-02-01 11:54:23 -0500

    subiendo mejoras al modulo de contrato

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -533,9 +533,10 @@
     @endif
 
     @if(Auth::user()->empresa()->adminOLT && isset($_SESSION['permisos']['761']))
+
     <li class="nav-item">
 
-        <a  class="nav-link" href="{{Auth::user()->empresa()->adminOLT}}">
+        <a  class="nav-link" href="{{Auth::user()->empresa()->adminOLT}}" target="_blank">
             <i class="menu-icon fas fa-server"></i>
             <span class="menu-title">Admin OLT</span>
         </a>
