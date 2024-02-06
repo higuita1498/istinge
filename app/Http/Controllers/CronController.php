153ab06622c712488d73732b42fe5ddaa2488d99
higuita1498 <higuitajokers@gmail.com>
2024-02-05 23:33:10 -0500

    Contratos indivisuales salian multiples

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -246,7 +246,7 @@ class CronController extends Controller
                                         if($contrato->factura_individual == 0){
                                             $contratos_multiples = Contrato::where('client_id',$factura->cliente)->where('factura_individual', 0)->get();
                                         }else {
-                                            $contratos_multiples = Contrato::where('client_id',$factura->cliente)->get();
+                                            $contratos_multiples = Contrato::where('nro',$contrato->nro)->where('client_id',$factura->cliente)->get();
                                         }
 
                                         foreach($contratos_multiples as $cm){
