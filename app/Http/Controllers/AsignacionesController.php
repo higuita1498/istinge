1151ab42ec54ecc18d033dcbed4bd8f87e3fd858
Reywis <reywisveliz@gmail.com>
2024-02-08 13:51:42 -0500

    subiendo mejoras para el modulo de asiganciones

--- app/Http/Controllers/AsignacionesController.php
+++ app/Http/Controllers/AsignacionesController.php
@@ -84,7 +84,7 @@ class AsignacionesController extends Controller
                 $contrato_nuevo->client_id = $request->id;
                 $contrato_nuevo->nro = $ultimoRegistro->nro + 1;
                 $contrato_nuevo->contrato_permanencia_meses = $request->contrato_permanencia_meses + 1;
-                $contrato_nuevo->plan_id = $request->plan;
+                $contrato_nuevo->plan_id = $request->plan_id;
                 $contrato_nuevo->server_configuration_id = 1;
                 $contrato_nuevo->servicio = $this->normaliza($servicio).'-'.($ultimoRegistro->nro + 1);
                 $contrato_nuevo->save();
