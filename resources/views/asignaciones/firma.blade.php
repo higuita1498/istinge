0626fc315c2998cf5b53be6347046fc69ca63341
Reywis <reywisveliz@gmail.com>
2024-02-08 13:09:01 -0500

    subiendo mejoras para el modulo de asiganciones

--- resources/views/asignaciones/firma.blade.php
+++ resources/views/asignaciones/firma.blade.php
@@ -27,14 +27,12 @@
         <input name="id" type="hidden" value="{{ $contacto->id }}">
         @csrf
         @php
-            $appUrl = env('APP_URL');
-            $rutaPdf = "/empresa/asignaciones/{$contacto->id}/imprimir";
-            $urlPdf = "{$appUrl}{$rutaPdf}";
+            $dominio = auth()->user()->empresa()->dominio;
         @endphp
         {{-- Imprime o debuggea las variables --}}
-       {{ auth()->user()->empresa()->dominio}}
+
             <div>
-                <embed src="{{ auth()->user()->empresa()->dominio }}/software/empresa/asignaciones/{{ $contacto->id }}/imprimir" type="application/pdf" width="100%" height="800px" style="margin-bottom:50px;"/>
+                <embed src="{{ $dominio }}/software/empresa/asignaciones/{{ $contacto->id }}/imprimir" type="application/pdf" width="100%" height="800px" style="margin-bottom:50px;"/>
             </div>
 
             <center>
