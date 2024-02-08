8a2203104af5de128a05d11129f3407c0889182c
Reywis <reywisveliz@gmail.com>
2024-02-08 13:23:56 -0500

    subiendo mejoras para el modulo de asiganciones

--- resources/views/asignaciones/firma.blade.php
+++ resources/views/asignaciones/firma.blade.php
@@ -28,7 +28,7 @@
         @csrf
 
         {{-- Imprime o debuggea las variables --}}
-
+        {{$empresa->dominio}}
             <div>
                 <embed src="{{ $empresa->dominio }}/software/empresa/asignaciones/{{ $contacto->id }}/imprimir" type="application/pdf" width="100%" height="800px" style="margin-bottom:50px;"/>
             </div>
