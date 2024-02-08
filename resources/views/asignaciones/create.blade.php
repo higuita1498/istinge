0cf094bab457bc526790cb075aa4a0946bc6ca32
Reywis <reywisveliz@gmail.com>
2024-02-08 09:27:43 -0500

    subiendo mejoras para el modulo de asiganciones

--- resources/views/asignaciones/create.blade.php
+++ resources/views/asignaciones/create.blade.php
@@ -149,7 +149,7 @@
                 <div class="col-md-3 form-group">
                     <label class="control-label">Plan <span class="text-danger">*</span></label>
                     <div class="input-group">
-                        <select class="form-control selectpicker" name="server_configuration_id" id="server_configuration_id" required="" title="Seleccione" data-live-search="true" data-size="5" onchange="getPlanes(this.value);">
+                        <select class="form-control selectpicker" name="server_configuration_id" id="server_configuration_id" required="" title="Seleccione" data-live-search="true" data-size="5">
                             @foreach($planes as $plan)
                                 <option value="{{$plan->id}}">{{$plan->name}}</option>
                             @endforeach
@@ -163,7 +163,7 @@
                 <div class="col-md-3 form-group" id="div_meses">
                     <label class="control-label">Meses del contrato de permanencia</span></label>
                     <div class="input-group">
-                        <select class="form-control selectpicker" id="contrato_permanencia_meses" name="contrato_permanencia_meses"  required="" title="Seleccione" data-live-search="true" data-size="5">
+                        <select class="form-control selectpicker" id="contrato_permanencia_meses" name="contrato_permanencia_meses" title="Seleccione" data-live-search="true" data-size="5">
                             <option value="3" {{old('contrato_permanencia_meses')==3?'selected':''}}>3 meses</option>
                             <option value="6" {{old('contrato_permanencia_meses')==6?'selected':''}}>6 meses</option>
                             <option value="9" {{old('contrato_permanencia_meses')==9?'selected':''}}>9 meses</option>
