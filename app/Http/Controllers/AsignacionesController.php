9f62f6f4378e59128d20a5004d7f8fed37ad4074
Reywis <reywisveliz@gmail.com>
2024-02-12 12:40:23 -0500

    verificando el error de asignaciones

--- app/Http/Controllers/AsignacionesController.php
+++ app/Http/Controllers/AsignacionesController.php
@@ -1455,7 +1455,6 @@ class AsignacionesController extends Controller
             return response()->json([
                 'success'  => true,
                 'contacto' => $contacto->id,
-                'empresa' => $empresa,
                 'text'     => "<a href='".config('app.url')."/api/contrato-digital/".$ref."' target='_blank'>".config('app.url')."/api/contrato-digital/".$ref."</a><br><br><button class='btn btn-primary btn-lg' data-clipboard-text='".$link."'>COPIAR URL</button>
                 ",
                 'link'     => config('app.url')."/api/contrato-digital/".$ref,
