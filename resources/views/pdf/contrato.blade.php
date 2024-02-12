74af78662a7a7aa69d7aaaff03164b291cbbd511
Reywis <reywisveliz@gmail.com>
2024-02-12 13:06:49 -0500

    verificando el error de asignaciones

--- resources/views/pdf/contrato.blade.php
+++ resources/views/pdf/contrato.blade.php
@@ -102,7 +102,7 @@
                 <tr>
                     <td>
                         <div style="margin-top: 10px; text-align:center;">
-                            <img src="{{asset('images/Empresas/Empresa'.Auth::user()->empresa.'/'.Auth::user()->empresa()->logo)}}" alt="" style="width: 130px !important;">
+                            {{-- <img src="{{asset('images/Empresas/Empresa'.Auth::user()->empresa.'/'.Auth::user()->empresa()->logo)}}" alt="" style="width: 130px !important;"> --}}
                         </div>
 
                         <div style="width: 100%; background-color: {{Auth::user()->empresa()->color}}; clear:both;  margin-top: 10px;">
