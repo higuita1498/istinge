24dc26f4ed967d4a36dbcc06a2d094f7dd45c84c
Reywis <reywisveliz@gmail.com>
2024-02-13 10:43:47 -0500

    revisando el pdf de asignaciones

--- resources/views/pdf/contrato.blade.php
+++ resources/views/pdf/contrato.blade.php
@@ -64,7 +64,9 @@
             font-size: 10px;line-height: 10px;margin: 0;
         }
         .titulo-bg {
-            background-color: {{Auth::user()->empresa()->color}};
+            /* background-color: {{Auth::user()->empresa()->color}}; */
+            background-color: {{$empresa->color}};
+
             color: white;
             padding: 5px;
             font-size: 12px;
