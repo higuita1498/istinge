e4b419667c3a74524e10fd5909342a69260cb737
juanjosetuiran1 <reywisveliz@gmail.com>
2024-04-01 13:39:49 -0500

    subiendo mejoras del reposrtes crc

--- resources/views/reportes/mintic/index.blade.php
+++ resources/views/reportes/mintic/index.blade.php
@@ -36,6 +36,40 @@
                     </div>
                 </div>
 
+                <label></label>
+                <select class="form-control selectpicker" name="fechas" id="fechas">
+                    <optgroup label="Presente">
+                        <option value="0">Hoy</option>
+                        <option value="1">Este Mes</option>
+                        <option value="2">Este Año</option>
+                    </optgroup>
+                    <optgroup label="Anterior">
+                        <option value="3">Ayer</option>
+                        <option value="4">Semana Pasada</option>
+                        <option value="5">Mes Anterior</option>
+                        <option value="6">Año Anterior</option>
+                    </optgroup>
+                    <optgroup label="Manual">
+                        <option value="7">Manual</option>
+                    </optgroup>
+                    <optgroup label="Todas">
+                        <option value="8">Todas</option>
+                    </optgroup>
+                </select>
+            </div>
+            <div class="form-group col-md-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <label>Desde <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control"  id="desde" value="{{$request->fecha}}" name="fecha" required="" >
+                    </div>
+                    <div class="col-md-6">
+                        <label >Hasta <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="hasta" value="{{$request->hasta}}" name="hasta" required="">
+                    </div>
+
+                </div>
+            </div>
             <div class="form-group col-md-4 text-center offset-md-4">
                 <center><button type="button" id="generar" class="btn btn-outline-primary">Generar Reporte</button>
                 <button type="button" id="exportar" class="btn btn-outline-success">Exportar a Excel</button></center>
