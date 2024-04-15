dc7309280a6db06f165a992b921f5fbd138f2382
juanjosetuiran1 <reywisveliz@gmail.com>
2024-04-15 11:17:20 -0500

    subiendo mejoras ip disponibles

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1681,7 +1681,8 @@ class Controller extends BaseController
                 // Se estaba generando el problema de que la codificación del
                 // arreglo y de los valores no llegaba correctamente, por eso
                 // es necesario convertir nuevamente el arreglo a UTF-8.
-                $sanitizedArray = mb_convert_encoding($ARRAY, "UTF-8", "auto");
+                $sanitizedArray = mb_convert_encoding($ARRAY, "UTF-8", mb_detect_encoding($ARRAY));
+                //$sanitizedArray = mb_convert_encoding($ARRAY, "UTF-8", "auto");
                 return response()->json(['software' => $ips, 'mikrotik' => $sanitizedArray]);
             }
         }
