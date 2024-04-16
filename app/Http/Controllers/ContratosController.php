88df94020b34d0af2fb7f2380c96443f5567d6a1
juanjosetuiran1 <reywisveliz@gmail.com>
2024-04-16 09:53:53 -0500

    subiendo mejoras en la exportacion de excel

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2215,7 +2215,7 @@ class ContratosController extends Controller
             $servicio_otro = $contrato->producto_exportar('servicio_otro');
 
             isset($plan->precio) ? $totalPlan+=$plan->precio : '';
-            $plan->precio=  round($plan->precio);
+
             isset($servicio->precio) ? $totalServicio+=$servicio->precio : '';
             isset($servicio_otro->precio) ? $totalServicioOtro+=$servicio_otro->precio : '';
 
@@ -2244,7 +2244,7 @@ class ContratosController extends Controller
                 ->setCellValue($letras[22].$i, $contrato->iva_factura == null || $contrato->iva_factura == 0 ? 'No' : 'Si')
                 ->setCellValue($letras[23].$i, $contrato->descuento != null ? $contrato->descuento . '%' : '0%' )
                 ->setCellValue($letras[24].$i, isset($plan->nombre) ? $plan->nombre : '')
-                ->setCellValue($letras[25].$i, isset($plan->nombre) ? number_format($plan->precio, 0, ',', '.') : '')
+                ->setCellValue($letras[25].$i, isset($plan->precio) ? $plan->precio : '')
                 ->setCellValue($letras[26].$i, isset($servicio->nombre) ? $servicio->nombre . " - $" . number_format($servicio->precio, 0, ',', '.') : '' )
                 ->setCellValue($letras[27].$i, isset($servicio_otro->nombre) ? $servicio_otro->nombre . " - $" . number_format($servicio_otro->precio, 0, ',', '.') : '' )
                 ;
