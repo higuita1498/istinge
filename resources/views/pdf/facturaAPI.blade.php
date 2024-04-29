4ea85b0494c3462f8a955d066dd55c36670efeb8
juanjosetuiran1 <reywisveliz@gmail.com>
2024-04-29 16:26:20 -0500

    factura

--- resources/views/pdf/facturaAPI.blade.php
+++ resources/views/pdf/facturaAPI.blade.php
@@ -190,7 +190,7 @@
 
                 @php $cont++; @endphp
                 <tr>
-                    <td class="left padding-left border_left @if($cont==$itemscount && $cont>6) border_bottom @endif">{{$item->producto()}} @if($item->descripcion) ({{$item->descripcion}}) @endif</td>
+                    <td class="left padding-left border_left @if($cont==$itemscount && $cont>6) border_bottom @endif">{{strtolower($item->producto())}} @if($item->descripcion) ({{strtolower($item->producto())}}) @endif</td>
                     <td class="center @if($cont==$itemscount && $cont>6) border_bottom @endif">{{$item->ref}}</td>
                     <td class="center  @if($cont==$itemscount && $cont>6) border_bottom @endif">{{$item->cant}}</td>
                     <td class="right padding-right  @if($cont==$itemscount && $cont>6) border_bottom @endif">{{$empresa->moneda}}{{App\Funcion::ParsearAPI($item->precio, $empresa->id)}}</td>
