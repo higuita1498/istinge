a5d7051cb74f801ed59e6c1836fa4e38e2f5d9c5
juanjosetuiran1 <reywisveliz@gmail.com>
2024-05-29 09:27:33 -0500

    revision sobre error de radicados

--- app/Http/Controllers/RadicadosController.php
+++ app/Http/Controllers/RadicadosController.php
@@ -189,11 +189,11 @@ class RadicadosController extends Controller{
         if($estado == 0){
 
             $radicados->where(function ($query) use ($estado) {
-                $query->whereIn('radicados.estatus', [1,3]);
+                $query->whereIn('radicados.estatus', [0,2]);
             });
         }elseif($estado == 1){
             $radicados->where(function ($query) use ($estado) {
-                $query->whereIn('radicados.estatus', [0,2]);
+                $query->whereIn('radicados.estatus', [1,3]);
             });
         }
 
