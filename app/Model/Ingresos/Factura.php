87be52ea255ae829ff2f70017324083221366aed
higuita1498 <higuitajokers@gmail.com>
2024-07-04 10:42:32 -0500

    fix: coprreccion error de saldo mes anterior en facturas solamente abiertas.

--- app/Model/Ingresos/Factura.php
+++ app/Model/Ingresos/Factura.php
@@ -870,7 +870,7 @@ public function forma_pago()
         /*>>>>>>>>>>>>>>>>>>>>>>>>>> Saldo mes Anterior <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
         //traemos todas las facturas que el vencimiento haya pasado la fecha actual.
-        $facturasVencidas = Factura::where('cliente',$this->cliente)->where('vencimiento','<',$fechaActual)->where('estatus','!=',2)->get();
+        $facturasVencidas = Factura::where('cliente',$this->cliente)->where('vencimiento','<',$fechaActual)->where('estatus','=',1)->get();
 
         //sumamos todo lo que deba el cliente despues de la fecha de vencimiento
         foreach($facturasVencidas as $vencida){
