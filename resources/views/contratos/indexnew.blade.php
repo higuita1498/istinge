2f1f90d5a66a52a6312897233388f221c695ce0a
juanjosetuiran1 <reywisveliz@gmail.com>
2024-08-02 14:58:01 -0500

    subiendo cambios nuevo para la exportacion de radicados

--- resources/views/contratos/indexnew.blade.php
+++ resources/views/contratos/indexnew.blade.php
@@ -115,14 +115,14 @@
         				<div class="col-md-3 pl-1 pt-1">
         					<select title="Planes" class="form-control selectpicker" id="plan" name="plan" data-size="5" data-live-search="true">
         						@foreach ($planes as $plan)
-        						<option value="{{ $plan->id }}">{{ $plan->producto }}</option>
+        						<option value="{{ $plan->id }}">{{ $plan->name }}</option>
         						@endforeach
         					</select>
         				</div>
                         <div class="col-md-3 pl-1 pt-1">
-        					<select title="Planes" class="form-control selectpicker" id="plan_tv" name="plan_tv" data-size="5" data-live-search="true">
+        					<select title="Planes de TV" class="form-control selectpicker" id="plan_tv" name="plan_tv" data-size="5" data-live-search="true">
         						@foreach ($planestv as $plan)
-        						<option value="{{ $plan->id }}">{{ $plan->name }}</option>
+        						<option value="{{ $plan->id }}">{{ $plan->producto  }}</option>
         						@endforeach
         					</select>
         				</div>
