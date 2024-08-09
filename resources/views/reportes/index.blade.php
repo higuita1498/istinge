f2f0883eac185d14c23163461a6f3a36f2f55a08
juanjosetuiran1 <reywisveliz@gmail.com>
2024-08-09 16:16:28 -0500

    cambianod el mensaje para envio sms telepon

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -55,8 +55,6 @@
             </ul>
         </div>
 
-
-
         <div class="row">
             <div class="col-md-5 offset-md-1 reportes">
                 <h3><i class="fas fa-chart-line"></i> Contables</h3>
@@ -67,7 +65,7 @@
 
         </div>
         <div class="col-md-5 offset-md-1 reportes">
-            <div class="col-md-5 offset-md-1 reportes">
+            <div class="col-md-8 offset-md-1 reportes">
                 <h3><i class="fa fa-gavel" aria-hidden="true"></i> REPORTES PASARELA DE PAGO</h3>
                 <ul class="list-report">
                     <li><a href="{{route('reportes.mintic.1.2')}}">Reporte Pasarela Combo pay</a></li>
