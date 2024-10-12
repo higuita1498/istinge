1a8cecfb08e299b7c4b3d1f72cfed4e7b1a4f899
higuita1498 <higuitajokers@gmail.com>
2024-10-12 01:57:55 -0500

    feat: actualizaciones unauthorize onus form y controladores 100%

--- resources/views/olt/unconfigured.blade.php
+++ resources/views/olt/unconfigured.blade.php
@@ -1,191 +1,191 @@
 @extends('layouts.app')
+
 @section('content')
-	<form method="POST" action="{{ route('olt.authorized-onus') }}" style="padding: 2% 3%;" role="form" class="forms-sample" novalidate id="form-retencion" >
-	   {{ csrf_field() }}
-	   <div class="row">
-        <div class="col-md-3 form-group">
-            <label class="control-label">OLT <span class="text-danger">*</span></label>
-            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="olt_id" id="olt_id">
-                @foreach($olts as $olt)
-                <option value="{{$olt['id']}}" selected>{{ $olt['name'] }}</option>
-                @endforeach
-            </select>
-            <span class="help-block error">
-                <strong>{{ $errors->first('olt_id') }}</strong>
-            </span>
+    @if(Session::has('success'))
+        <div class="alert alert-success">
+            {{Session::get('success')}}
         </div>
-	        <div class="col-md-3 form-group">
-	            <label class="control-label">Pon Type <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control" name="pon_type" id="pon_type" value="{{$request->ponType}}" readonly>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('pon_type') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Board <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control" name="board" id="board" value="{{$request->board}}" readonly>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('board') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Port <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control" name="port" id="port" value="{{$request->port}}" readonly>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('port') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Sn <span class="text-danger">*</span></label>
-	            <input type="text" class="form-control" name="sn" id="sn" value="{{$request->sn}}" readonly>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('sn') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">ONU TYpe <span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="onu_type" id="onu_type">
-                    @foreach($onu_types as $type)
-                    <option value="{{$type['name']}}">{{ $type['name'] }}</option>
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('onu_type') }}</strong>
-	            </span>
-	        </div>
+        <script type="text/javascript">
+            setTimeout(function() {
+                $('.alert').hide();
+                $('.active_table').attr('class', ' ');
+            }, 5000);
+        </script>
+    @endif
 
-            <div class="col-md-3 form-group">
-                    <label class="control-label">ONU mode  </label>
-                  <div class="row">
-                      <div class="col-sm-6">
-                      <div class="form-radio">
-                          <label class="form-check-label">
-                          <input type="radio" class="form-check-input" name="onu_mode" id="routing" value="Routing" checked> Routing
-                          <i class="input-helper"></i><i class="input-helper"></i></label>
-                      </div>
-                  </div>
-                  <div class="col-sm-6">
-                      <div class="form-radio">
-                          <label class="form-check-label">
-                          <input type="radio" class="form-check-input" name="onu_mode" id="bridging" value="Bridging"> Bridging
-                          <i class="input-helper"></i><i class="input-helper"></i></label>
-                      </div>
-                  </div>
-                  </div>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">User VLAN-ID <span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="user_vlan_id" id="user_vlan_id">
-                    @foreach($vlan as $vl)
-                    <option value="{{$vl['id']}}">{{ $vl['vlan'] }} {{ $vl['description'] != "" ? " - " . $vl['description'] : '' }}</option>
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('user_vlan_id') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Zone<span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="zone" id="zone">
-                    @foreach($zones as $zone)
-                    <option value="{{ $zone['name'] }}" {{ $default_zone == $zone['id'] ? 'selected' : '' }}>{{ $zone['name'] }}</option>
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('zone') }}</strong>
-	            </span>
-	        </div>
+    @if(Session::has('error'))
+        <div class="alert alert-danger" >
+            {{Session::get('error')}}
+        </div>
 
-            <div class="col-md-3 form-group">
-	            <label class="control-label">ODB (Splitter)</label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="odb_splitter" id="odb_splitter">
-                    <option value="0">None</option>
-                    @foreach($odbList as $odbSplitter)
-                    @if ($odbSplitter['nr_of_ports'] == null)
-                    <option value="{{ $odbSplitter['name'] }}">{{ $odbSplitter['name'] }}</option>
-                    @endif
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('odb_splitter') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">ODB Port</label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="odb_port" id="odb_port">
-                    <option value="0">None</option>
-                    @foreach($odbList as $odbPort)
-                    @if ($odbPort['nr_of_ports'] != null)
-                    <option value="{{ $odbPort['id'] }}">{{ $odbPort['name'] }}</option>
-                    @endif
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('odb_port') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Download speed<span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="download_speed" id="download_speed">
-                    @foreach($speedProfiles as $speedDownload)
-                    @if ($speedDownload['direction'] == 'download')
-                    <option value="{{ $speedDownload['name'] }}">{{ $speedDownload['name'] }} - {{ $speedDownload['speed'] }}</option>
-                    @endif
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('download_speed') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Upload speed<span class="text-danger">*</span></label>
-	            <select class="form-control selectpicker" data-live-search="true" data-size="5" name="upload_speed" id="upload_speed">
-                    @foreach($speedProfiles as $speedUpload)
-                    @if ($speedUpload['direction'] == 'upload')
-                    <option value="{{ $speedUpload['name'] }}">{{ $speedUpload['name'] }} - {{ $speedUpload['speed'] }}</option>
-                    @endif
-                    @endforeach
-                </select>
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('upload_speed') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">Name<span class="text-danger">*</span></label>
-	            <input type="text" class="form-control" name="name" id="name">
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('upload_speed') }}</strong>
-	            </span>
-	        </div>
-	        <div class="col-md-6 form-group">
-	            <label class="control-label">Address or comment </label>
-	            <input type="text" name="address_comment" id="address_comment" class="form-control">
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('address_comment') }}</strong>
-	            </span>
-	        </div>
-            <div class="col-md-3 form-group">
-	            <label class="control-label">ONU external ID <span class="text-danger">*</span></label>
-	            <input type="text" name="onu_external_id" id="onu_external_id" class="form-control" value="{{ $request->sn }}">
-	            <span class="help-block error">
-	                <strong>{{ $errors->first('onu_external_id') }}</strong>
-	            </span>
-	        </div>
-	   </div>
+        <script type="text/javascript">
+            setTimeout(function(){
+                $('.alert').hide();
+                $('.active_table').attr('class', ' ');
+            }, 8000);
+        </script>
+    @endif
 
-	   <small>Los campos marcados con <span class="text-danger">*</span> son obligatorios</small>
-	   <hr>
-	   <div class="row" >
-	       <div class="col-sm-12" style="text-align: right;  padding-top: 1%;">
-	           <a href="{{route('mikrotik.index')}}" class="btn btn-outline-secondary">Cancelar</a>
-	           <button type="submit" id="submitcheck" onclick="submitLimit(this.id)" class="btn btn-success">Guardar</button>
-	       </div>
-	   </div>
-    </form>
+    @if(Session::has('danger'))
+        <div class="alert alert-danger">
+            {{Session::get('danger')}}
+        </div>
+        <script type="text/javascript">
+            setTimeout(function() {
+                $('.alert').hide();
+                $('.active_table').attr('class', ' ');
+            }, 5000);
+        </script>
+    @endif
+
+    @if(Session::has('message_denied'))
+        <div class="alert alert-danger" role="alert">
+            {{Session::get('message_denied')}}
+            @if(Session::get('errorReason'))<br> <strong>Razon(es): <br></strong>
+            @if(count(Session::get('errorReason')) > 1)
+                @php $cont = 0 @endphp
+                @foreach(Session::get('errorReason') as $error)
+                    @php $cont = $cont + 1; @endphp
+                    {{$cont}} - {{$error}} <br>
+                @endforeach
+            @endif
+            @endif
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if(Session::has('message_success'))
+        <div class="alert alert-success" role="alert">
+            {{Session::get('message_success')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    <div class="row card-description">
+        <div class="col-md-12">
+            <table class="table table-striped table-hover" id="table-general">
+                <thead class="thead-dark">
+                <tr>
+                    <th width='3%'>PON Type</th>
+                    <th width='3%'>Board</th>
+                    <th width='3%'>Port</th>
+                    <th width='1%'>Pon Description</th>
+                    <th width='3%'>SN</th>
+                    <th width='3%'>Type</th>
+                    <th width='3%'>Estatus</th>
+                    <th width='3%'>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @for ($i=0; $i < count($onus); $i++)
+                    <tr id="olt_{{$i}}">
+                        <td>{{ $onus[$i]['pon_type'] }}</td>
+                        <td>{{ $onus[$i]['board'] }}</td>
+                        <td>{{ $onus[$i]['port'] }}</td>
+                        <td>{{ $onus[$i]['pon_description'] }}</td>
+                        <td>{{ isset($onus[$i]['sn']) ? $onus[$i]['sn'] : '' }}</td>
+                        <td>{{ $onus[$i]['onu_type_name'] }}</td>
+                        <td>{{ $onus[$i]['is_disabled'] == 1 ? 'Innactivo' : 'Activo' }}</td>
+                        <td>
+                            @if($onus[$i]['is_disabled'] == 0)
+                            {{-- <a href="#" onclick="authorizeOnu({{$i}})">Authorize</a> --}}
+                            <a href="#" onclick="formAuthorizeOnu({{$i}})">Authorize</a>
+                            @endif
+                        </td>
+                    </tr>
+                @endfor
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 @endsection
-
 @section('scripts')
     <script>
 
+        function formAuthorizeOnu(index){
+
+            let row = document.getElementById('olt_' + index);
+
+            let ponType = row.cells[0].innerText;
+            let board = row.cells[1].innerText;
+            let port = row.cells[2].innerText;
+            let ponDescription = row.cells[3].innerText;
+            let sn = row.cells[4].innerText;
+            let onuTypeName = row.cells[5].innerText;
+            let status = row.cells[6].innerText;
+
+            // Construir la URL con los parámetros GET
+            let url = `{{ route('olt.form-authorized-onus') }}?ponType=${encodeURIComponent(ponType)}&board=${encodeURIComponent(board)}&port=${encodeURIComponent(port)}&ponDescription=${encodeURIComponent(ponDescription)}&sn=${encodeURIComponent(sn)}&onuTypeName=${encodeURIComponent(onuTypeName)}&status=${encodeURIComponent(status)}`;
+
+            // Redirigir a la URL
+            window.location.href = url;
+
+        }
+
+        function authorizeOnu(index){
+
+            if (window.location.pathname.split("/")[1] === "software") {
+				var url='/software/Olt/authorized-onus';
+			}else{
+				var url = '/Olt/authorized-onus';
+			}
+
+            let row = document.getElementById('olt_' + index);
+
+            let ponType = row.cells[0].innerText;
+            let board = row.cells[1].innerText;
+            let port = row.cells[2].innerText;
+            let ponDescription = row.cells[3].innerText;
+            let sn = row.cells[4].innerText;
+            let onuTypeName = row.cells[5].innerText;
+            let status = row.cells[6].innerText;
+
+            $.ajax({
+                url: url,
+                headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                method: 'get',
+                data: {
+                    ponType,
+                    board,
+                    port,
+                    ponDescription,
+                    sn,
+                    onuTypeName,
+                    status
+                },
+                success: function (data) {
+
+					// if(data.status == '200'){
+					// 	Swal.fire({
+                	// 	type: 'success',
+                	// 	title: 'La ONU ha sido autorizada correctamente.',
+                	// 	text: 'Recargando la página',
+                	// 	showConfirmButton: false,
+                	// 	timer: 5000
+                	// 	})
+					// }else{
+					// 	Swal.fire({
+                	// 	type: 'error',
+                	// 	title: 'La ONU no pudo ser autorizada.',
+                	// 	text: 'Recargando la página',
+                	// 	showConfirmButton: false,
+                	// 	timer: 5000
+                	// 	})
+					// }
+
+                    // setTimeout(function(){
+                    // 	var a = document.createElement("a");
+                    // 	a.href = window.location.pathname;
+                    // 	a.click();
+                    // }, 2000);
+                }
+            });
+
+        }
     </script>
 @endsection
