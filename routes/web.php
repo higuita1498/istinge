d82237a3d664355f0857bfc736aa008279662d38
higuita1498 <higuitajokers@gmail.com>
2024-10-12 01:55:07 -0500

    feat: actualizaciones unauthorize onus form y controladores 100%

--- routes/web.php
+++ routes/web.php
@@ -321,7 +321,7 @@ Route::group(['prefix' => 'tecnico', 'middleware' => ['auth']], function() {
 });
 
 Route::group(['prefix' => 'Olt'], function(){
-    Route::get('unconfigured-onus','OltController@unConfiguredOnus')->name('olt.unconfigured');
+    Route::get('unconfigured-onus','OltController@unConfiguredOnus_view')->name('olt.unconfigured');
     Route::post('authorized-onus','OltController@authorizedOnus')->name('olt.authorized-onus');
     Route::get('form-authorized-onu','OltController@formAuthorizeOnu')->name('olt.form-authorized-onus');
 });
