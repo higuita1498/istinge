98454e9db971104eb0aeaf9188eb0266bd1db91d
Johan Carrillo <johancar991@gmail.com>
2024-11-28 15:34:43 -0500

    fix: Se actualiza valor a 0 cuando se anula un pago

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -218,6 +218,11 @@ class Controller extends BaseController
 
         if ($regis) {
             $movimiento=$regis;
+
+            if($modulo == 1){
+                $movimiento->saldo = 0;
+            }
+
             $movimiento->estatus=$estatus;
             $movimiento->save();
         }
