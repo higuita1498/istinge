3ed1b51fb0a259f685873d2b5d4044cdc3c2d105
Johan Carrillo <johancar991@gmail.com>
2024-12-02 17:08:23 -0500

    fix: Se edita el show de facturas, añadiendo mostrar contratos habilitados e inhabilitados

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -526,7 +526,7 @@ class ContactosController extends Controller
             }
 
             $user_app = DB::table('usuarios_app')->where('id_cliente', $contacto->id)->where('status', 1)->first();
-            $contratos = Contrato::where('client_id', $contacto->id)->where('status', 1)->get();
+            $contratos = Contrato::where('client_id', $contacto->id)->get();
 
             return view('contactos.show')->with(compact('contacto', 'id', 'user_app', 'contratos'));
         }
