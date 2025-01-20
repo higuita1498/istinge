7a37fba636adeeaa9f03e7152ad98fb4f93cbb29
higuita1498 <higuitajokers@gmail.com>
2025-01-20 10:18:05 -0500

    fix: correccion de barrios 2.0

--- resources/views/pdf/radicados.blade.php
+++ resources/views/pdf/radicados.blade.php
@@ -183,7 +183,7 @@
                             <th style="text-align: left; padding-left: 9px;">Barrio</th>
                             <td style="padding-left: 9px;">{{$radicado->barrio}}</td>
                         </tr>
-                        @elseif($radicado->cliente()->barrio())
+                        @elseif($radicado->cliente()->barrio()->nombre)
                         <tr>
                             <th style="text-align: left; padding-left: 9px;">Barrio</th>
                             <td style="padding-left: 9px;">{{$radicado->cliente()->barrio()}}</td>
